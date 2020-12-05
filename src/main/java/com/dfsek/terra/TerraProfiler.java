264f1df172f38b7b173833f448c662a0ee54f110
dfsek <dfsek@protonmail.com>
2020-12-05 16:13:17 -0700

    Update profiler target times

--- src/main/java/com/dfsek/terra/TerraProfiler.java
+++ src/main/java/com/dfsek/terra/TerraProfiler.java
@@ -16,9 +16,9 @@ public class TerraProfiler extends WorldProfiler {
         super(w);
         this
                 .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "FloraTime")
-                .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "TreeTime")
+                .addMeasurement(new Measurement(10000000, DataType.PERIOD_MILLISECONDS), "TreeTime")
                 .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "OreTime")
-                .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "CaveTime")
+                .addMeasurement(new Measurement(3500000, DataType.PERIOD_MILLISECONDS), "CaveTime")
                 .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "StructureTime")
                 .addMeasurement(new Measurement(1500000, DataType.PERIOD_MILLISECONDS), "ElevationTime");
     }
