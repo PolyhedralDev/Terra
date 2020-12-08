37c2d945ec2cda260e5b0729ba5a9d918b1f2815
dfsek <dfsek@protonmail.com>
2020-12-08 12:32:51 -0700

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val versionObj = Version("2", "0", "0", true)
+val versionObj = Version("2", "0", "1", true)
 
 version = versionObj
 
