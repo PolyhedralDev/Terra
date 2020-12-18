b33c8d6b77d5cec3ae15321a8d8cf85aeb3de633
dfsek <dfsek@protonmail.com>
2020-12-18 15:35:09 -0700

    fix population order

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/features/PopulatorFeature.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/features/PopulatorFeature.java
@@ -28,8 +28,8 @@ public class PopulatorFeature extends Feature<DefaultFeatureConfig> {
         FabricWorld world1 = new FabricWorld(world.toServerWorld(), new FabricChunkGenerator(chunkGenerator));
         gen.getCavePopulator().populate(world1, random, chunk);
         gen.getOrePopulator().populate(world1, random, chunk);
-        gen.getFloraPopulator().populate(world1, random, chunk);
         gen.getTreePopulator().populate(world1, random, chunk);
+        gen.getFloraPopulator().populate(world1, random, chunk);
         return true;
     }
 }
