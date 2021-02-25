7f988dcf267d8e3a8eff8f49f3e7d5d763399c68
dfsek <dfsek@protonmail.com>
2021-02-25 01:43:01 -0700

    implement FabricDirectional

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/FabricAdapter.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/FabricAdapter.java
@@ -1,6 +1,7 @@
 package com.dfsek.terra.fabric.world;
 
 import com.dfsek.terra.api.math.vector.Vector3;
+import com.dfsek.terra.api.platform.block.BlockFace;
 import com.dfsek.terra.api.platform.block.BlockType;
 import com.dfsek.terra.api.platform.entity.EntityType;
 import com.dfsek.terra.api.platform.inventory.item.Enchantment;
@@ -9,6 +10,7 @@ import com.dfsek.terra.fabric.inventory.FabricItem;
 import com.dfsek.terra.fabric.inventory.FabricItemStack;
 import com.dfsek.terra.fabric.world.block.FabricBlockData;
 import com.dfsek.terra.fabric.world.block.FabricBlockType;
+import com.dfsek.terra.fabric.world.block.data.FabricDirectional;
 import com.dfsek.terra.fabric.world.block.data.FabricMultipleFacing;
 import com.dfsek.terra.fabric.world.block.data.FabricOrientable;
 import com.dfsek.terra.fabric.world.block.data.FabricRotatable;
@@ -24,6 +26,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.WorldAccess;
 
 import java.util.Arrays;
@@ -42,9 +45,14 @@ public final class FabricAdapter {
 
         if(state.contains(Properties.SLAB_TYPE)) return new FabricSlab(state);
 
-        if(state.contains(Properties.AXIS)) return new FabricOrientable(state);
+        if(state.contains(Properties.AXIS)) return new FabricOrientable(state, Properties.AXIS);
+        if(state.contains(Properties.HORIZONTAL_AXIS)) return new FabricOrientable(state, Properties.HORIZONTAL_AXIS);
 
         if(state.contains(Properties.ROTATION)) return new FabricRotatable(state);
+
+        if(state.contains(Properties.FACING)) return new FabricDirectional(state, Properties.FACING);
+        if(state.contains(Properties.HOPPER_FACING)) return new FabricDirectional(state, Properties.HOPPER_FACING);
+        if(state.contains(Properties.HORIZONTAL_FACING)) return new FabricDirectional(state, Properties.HORIZONTAL_FACING);
 
         if(state.getProperties().containsAll(Arrays.asList(Properties.NORTH, Properties.SOUTH, Properties.EAST, Properties.WEST)))
             return new FabricMultipleFacing(state);
@@ -52,12 +60,27 @@ public final class FabricAdapter {
         return new FabricBlockData(state);
     }
 
-    public static BlockType adapt(Block block) {
-        return new FabricBlockType(block);
+    public static Direction adapt(BlockFace face) {
+        switch(face) {
+            case NORTH:
+                return Direction.NORTH;
+            case WEST:
+                return Direction.WEST;
+            case SOUTH:
+                return Direction.SOUTH;
+            case EAST:
+                return Direction.EAST;
+            case UP:
+                return Direction.UP;
+            case DOWN:
+                return Direction.DOWN;
+            default:
+                throw new IllegalArgumentException("Illegal direction: " + face);
+        }
     }
 
-    public WorldAccess adapt(FabricWorldHandle worldHandle) {
-        return worldHandle.getWorld();
+    public static BlockType adapt(Block block) {
+        return new FabricBlockType(block);
     }
 
     public static EntityType adapt(net.minecraft.entity.EntityType<?> entityType) {
@@ -86,5 +109,9 @@ public final class FabricAdapter {
 
     public static net.minecraft.enchantment.Enchantment adapt(Enchantment enchantment) {
         return ((FabricEnchantment) enchantment).getHandle();
+    }
+
+    public WorldAccess adapt(FabricWorldHandle worldHandle) {
+        return worldHandle.getWorld();
     }
 }
--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/block/data/FabricOrientable.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/world/block/data/FabricOrientable.java
@@ -4,15 +4,19 @@ import com.dfsek.terra.api.platform.block.Axis;
 import com.dfsek.terra.api.platform.block.data.Orientable;
 import com.dfsek.terra.fabric.world.block.FabricBlockData;
 import net.minecraft.block.BlockState;
-import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.math.Direction;
 
 import java.util.Arrays;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class FabricOrientable extends FabricBlockData implements Orientable {
-    public FabricOrientable(BlockState delegate) {
+    private final EnumProperty<Direction.Axis> property;
+
+    public FabricOrientable(BlockState delegate, EnumProperty<Direction.Axis> property) {
         super(delegate);
+        this.property = property;
     }
 
     @Override
@@ -22,11 +26,11 @@ public class FabricOrientable extends FabricBlockData implements Orientable {
 
     @Override
     public Axis getAxis() {
-        return FabricEnumAdapter.adapt(getHandle().get(Properties.AXIS));
+        return FabricEnumAdapter.adapt(getHandle().get(property));
     }
 
     @Override
     public void setAxis(Axis axis) {
-        delegate = delegate.with(Properties.AXIS, FabricEnumAdapter.adapt(axis));
+        delegate = delegate.with(property, FabricEnumAdapter.adapt(axis));
     }
 }
