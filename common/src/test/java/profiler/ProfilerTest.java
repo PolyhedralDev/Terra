b3e3c28276095ff401c29f5bdae4bee0a69943e0
dfsek <dfsek@protonmail.com>
2021-04-12 00:05:53 -0700

    implement TerraPlugin#getProfier

--- common/src/test/java/profiler/ProfilerTest.java
+++ common/src/test/java/profiler/ProfilerTest.java
@@ -1,11 +1,13 @@
 package profiler;
 
 import com.dfsek.terra.profiler.Profiler;
+import com.dfsek.terra.profiler.ProfilerImpl;
 
 public class ProfilerTest {
+    private static final Profiler PROFILER = new ProfilerImpl();
     //@Test
     public static void main(String... a) throws InterruptedException {
-        Profiler.INSTANCE.start();
+        PROFILER.start();
         for(int i = 0; i < 1000; i++) {
             doThing();
         }
@@ -17,29 +19,29 @@ public class ProfilerTest {
         for(int i = 0; i < 100; i++) {
             doOtherThing();
         }
-        Profiler.INSTANCE.stop();
-        Profiler.INSTANCE.getTimings().forEach((id, timings) -> {
+        PROFILER.stop();
+        PROFILER.getTimings().forEach((id, timings) -> {
             System.out.println(id + ": " + timings.toString());
         });
     }
 
     private static void doThing() throws InterruptedException {
-        Profiler.INSTANCE.push("thing");
+        PROFILER.push("thing");
         Thread.sleep(1);
         doOtherThing();
-        Profiler.INSTANCE.pop("thing");
+        PROFILER.pop("thing");
     }
 
     private static void doOtherThing() throws InterruptedException {
-        Profiler.INSTANCE.push("thing2");
+        PROFILER.push("thing2");
         Thread.sleep(2);
         doThirdOtherThing();
-        Profiler.INSTANCE.pop("thing2");
+        PROFILER.pop("thing2");
     }
 
     private static void doThirdOtherThing() throws InterruptedException {
-        Profiler.INSTANCE.push("thing3");
+        PROFILER.push("thing3");
         Thread.sleep(2);
-        Profiler.INSTANCE.pop("thing3");
+        PROFILER.pop("thing3");
     }
 }
