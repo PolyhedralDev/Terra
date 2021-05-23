3472859afb25ee0ab93ffccabb14f9e80992d814
dfsek <dfsek@protonmail.com>
2021-05-22 17:10:50 -0700

    remove unused event handlers

--- platforms/forge/src/main/java/com/dfsek/terra/forge/listener/RegistryListener.java
+++ platforms/forge/src/main/java/com/dfsek/terra/forge/listener/RegistryListener.java
@@ -1,8 +1,6 @@
 package com.dfsek.terra.forge.listener;
 
-import com.dfsek.terra.forge.ForgeUtil;
 import com.dfsek.terra.forge.TerraForgePlugin;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.Feature;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -10,13 +8,6 @@ import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class RegistryListener {
-    private static final TerraForgePlugin INSTANCE = TerraForgePlugin.getInstance();
-
-    @SubscribeEvent
-    public static void register(RegistryEvent.Register<Biome> event) {
-        INSTANCE.logger().info("Registering biomes...");
-    }
-
     @SubscribeEvent
     public static void registerPop(RegistryEvent.Register<Feature<?>> event) {
         event.getRegistry().register(TerraForgePlugin.POPULATOR_FEATURE);
