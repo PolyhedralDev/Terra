1a50060ff01d590b5e3650d731de56c402cd16b9
dfsek <dfsek@protonmail.com>
2021-07-05 01:00:52 -0700

    flora addon impl

---
+++ common/addons/palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
@@ -0,0 +1,27 @@
+package com.dfsek.terra.addons.palette;
+
+import com.dfsek.terra.api.TerraPlugin;
+import com.dfsek.terra.api.addon.TerraAddon;
+import com.dfsek.terra.api.addon.annotations.Addon;
+import com.dfsek.terra.api.addon.annotations.Author;
+import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.event.EventListener;
+import com.dfsek.terra.api.event.events.config.ConfigPackPreLoadEvent;
+import com.dfsek.terra.api.injection.annotations.Inject;
+
+@Addon("core-palette-config")
+@Author("Terra")
+@Version("1.0.0")
+public class PaletteAddon extends TerraAddon implements EventListener {
+    @Inject
+    private TerraPlugin main;
+
+    @Override
+    public void initialize() {
+        main.getEventManager().registerListener(this, this);
+    }
+
+    public void onPackLoad(ConfigPackPreLoadEvent event) {
+        event.getPack().registerConfigType(new PaletteConfigType(event.getPack(), main), "PALETTE", 2);
+    }
+}
