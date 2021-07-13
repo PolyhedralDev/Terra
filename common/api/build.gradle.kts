27d9716c5daf4318f1ce3b6d9ce530e58fa7edcf
dfsek <dfsek@protonmail.com>
2021-07-13 14:26:37 -0700

    update to Tectonic 2.0.0

--- common/api/build.gradle.kts
+++ common/api/build.gradle.kts
@@ -15,7 +15,7 @@ group = "com.dfsek.terra.common"
 dependencies {
 
     "shadedApi"("com.dfsek:Paralithic:0.3.2")
-    "shadedApi"("com.dfsek:Tectonic:1.4.0")
+    "implementation"("com.dfsek.tectonic:common:2.0.0")
 
     "shadedApi"("net.jafama:jafama:2.3.2")
     "shadedApi"("org.yaml:snakeyaml:1.27")
