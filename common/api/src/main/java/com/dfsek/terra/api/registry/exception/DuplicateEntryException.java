b3594b24797a5352a29970c19df22d38d5c98ede
dfsek <dfsek@protonmail.com>
2021-07-15 13:55:47 -0700

    make DuplicateEntryException unchecked

--- common/api/src/main/java/com/dfsek/terra/api/registry/exception/DuplicateEntryException.java
+++ common/api/src/main/java/com/dfsek/terra/api/registry/exception/DuplicateEntryException.java
@@ -3,7 +3,7 @@ package com.dfsek.terra.api.registry.exception;
 /**
  * Thrown when a duplicate entry is found in a registry.
  */
-public class DuplicateEntryException extends Exception {
+public class DuplicateEntryException extends RuntimeException {
     private static final long serialVersionUID = -7199021672428288780L;
 
     public DuplicateEntryException(String message) {
