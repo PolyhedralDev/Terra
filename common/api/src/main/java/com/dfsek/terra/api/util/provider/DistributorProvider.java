11f161d5468a09b2c220297c221a343432070205
dfsek <dfsek@protonmail.com>
2021-07-18 14:05:52 -0700

    remove DistributorProvider

--- common/api/src/main/java/com/dfsek/terra/api/util/provider/DistributorProvider.java
+++
@@ -1,9 +0,0 @@
-package com.dfsek.terra.api.util.provider;
-
-import com.dfsek.tectonic.loading.object.ObjectTemplate;
-import com.dfsek.terra.api.structure.feature.Distributor;
-
-import java.util.function.Supplier;
-
-public interface DistributorProvider extends Supplier<ObjectTemplate<Distributor>> {
-}
