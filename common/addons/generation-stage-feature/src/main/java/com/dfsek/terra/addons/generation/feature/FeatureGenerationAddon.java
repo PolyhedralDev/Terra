6b191ac00bc20496feb3e75562f59fd6b49d19e1
dfsek <dfsek@protonmail.com>
2021-07-17 19:49:42 -0700

    initial FeatureGenerationAddon impl

---
+++ common/addons/generation-stage-feature/src/main/java/com/dfsek/terra/addons/generation/feature/FeatureGenerationAddon.java
@@ -0,0 +1,22 @@
+package com.dfsek.terra.addons.generation.feature;
+
+import com.dfsek.terra.api.TerraPlugin;
+import com.dfsek.terra.api.addon.TerraAddon;
+import com.dfsek.terra.api.addon.annotations.Addon;
+import com.dfsek.terra.api.addon.annotations.Author;
+import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.event.EventListener;
+import com.dfsek.terra.api.injection.annotations.Inject;
+
+@Addon("generation-stage-feature")
+@Version("1.0.0")
+@Author("Terra")
+public class FeatureGenerationAddon extends TerraAddon implements EventListener {
+    @Inject
+    private TerraPlugin main;
+
+    @Override
+    public void initialize() {
+        main.getEventManager().registerListener(this, this);
+    }
+}
