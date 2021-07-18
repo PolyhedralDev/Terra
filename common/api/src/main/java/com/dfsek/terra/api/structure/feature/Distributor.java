f4f595425a5ab17358d9bdfd65651a47730184f8
dfsek <dfsek@protonmail.com>
2021-07-17 19:34:19 -0700

    Distributor api

--- common/api/src/main/java/com/dfsek/terra/api/structure/feature/Distributor.java
+++ common/api/src/main/java/com/dfsek/terra/api/structure/feature/Distributor.java
@@ -1,4 +1,13 @@
 package com.dfsek.terra.api.structure.feature;
 
 public interface Distributor {
+    boolean matches(int x, int z);
+
+    default Distributor and(Distributor other) {
+        return (x, z) -> this.matches(x, z) && other.matches(x, z);
+    }
+
+    default Distributor or(Distributor other) {
+        return (x, z) -> this.matches(x, z) || other.matches(x, z);
+    }
 }
