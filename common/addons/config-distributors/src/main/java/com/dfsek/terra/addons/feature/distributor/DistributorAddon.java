ced5a0537017f69cd3245403a87a92985a918f61
dfsek <dfsek@protonmail.com>
2021-07-17 21:33:52 -0700

    create base DistributorAddon

---
+++ common/addons/config-distributors/src/main/java/com/dfsek/terra/addons/feature/distributor/DistributorAddon.java
@@ -0,0 +1,24 @@
+package com.dfsek.terra.addons.feature.distributor;
+
+import com.dfsek.terra.api.TerraPlugin;
+import com.dfsek.terra.api.addon.TerraAddon;
+import com.dfsek.terra.api.addon.annotations.Addon;
+import com.dfsek.terra.api.addon.annotations.Author;
+import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.event.EventListener;
+import com.dfsek.terra.api.injection.annotations.Inject;
+
+@Addon("config-distributors")
+@Version("1.0.0")
+@Author("Terra")
+public class DistributorAddon extends TerraAddon implements EventListener {
+    @Inject
+    private TerraPlugin main;
+
+    @Override
+    public void initialize() {
+        main.getEventManager().registerListener(this, this);
+    }
+
+
+}
