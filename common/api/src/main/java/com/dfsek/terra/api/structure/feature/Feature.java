b52dcf2f15e6d8c35acc6a9e7a297f88736d3762
dfsek <dfsek@protonmail.com>
2021-07-17 19:37:14 -0700

    add world context to Feature

--- common/api/src/main/java/com/dfsek/terra/api/structure/feature/Feature.java
+++ common/api/src/main/java/com/dfsek/terra/api/structure/feature/Feature.java
@@ -1,9 +1,10 @@
 package com.dfsek.terra.api.structure.feature;
 
 import com.dfsek.terra.api.structure.Structure;
+import com.dfsek.terra.api.world.World;
 
 public interface Feature {
-    Structure getStructure(double x, double y, double z);
+    Structure getStructure(World world, int x, int y, int z);
 
     Distributor getDistributor();
 
