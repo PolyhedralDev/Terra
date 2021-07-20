3a7edcb1e4e2e77cd435eb8087224c9552c584dc
dfsek <dfsek@protonmail.com>
2021-07-20 15:18:03 -0700

    feature configuration

--- common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
+++ common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
@@ -6,6 +6,7 @@ import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
 import com.dfsek.terra.api.addon.annotations.Version;
 import com.dfsek.terra.api.event.EventListener;
+import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.injection.annotations.Inject;
 
 @Addon("config-feature")
@@ -18,5 +19,9 @@ public class FeatureAddon extends TerraAddon implements EventListener {
     @Override
     public void initialize() {
         main.getEventManager().registerListener(this, this);
+    }
+
+    public void onPackLoad(ConfigPackPreLoadEvent event) {
+        event.getPack().registerConfigType(new FeatureConfigType(), "FEATURE", 2);
     }
 }
