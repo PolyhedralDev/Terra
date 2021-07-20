96b52f9a3eda7b8d2ce0df1a841fa8be502b89c1
dfsek <dfsek@protonmail.com>
2021-07-19 21:20:59 -0700

    remove unused class ExpressionSampler

--- common/api/src/main/java/com/dfsek/terra/api/util/seeded/SeededNoiseSampler.java
+++ common/api/src/main/java/com/dfsek/terra/api/util/seeded/SeededNoiseSampler.java
@@ -3,19 +3,6 @@ package com.dfsek.terra.api.util.seeded;
 import com.dfsek.terra.api.noise.NoiseSampler;
 
 public interface SeededNoiseSampler extends SeededBuilder<NoiseSampler> {
-    static SeededNoiseSampler zero(int dimensions) {
-        return new SeededNoiseSampler() {
-            @Override
-            public NoiseSampler build(long seed) {
-                return NoiseSampler.zero();
-            }
-
-            @Override
-            public int getDimensions() {
-                return dimensions;
-            }
-        };
-    }
 
     int getDimensions();
 }
