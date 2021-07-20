c1dfcafa1d0feedcd25b869045e043f445296458
dfsek <dfsek@protonmail.com>
2021-07-20 15:06:23 -0700

    create base FeatureAddon

---
+++ common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
@@ -0,0 +1,22 @@
+package com.dfsek.terra.addons.feature;
+
+import com.dfsek.terra.api.TerraPlugin;
+import com.dfsek.terra.api.addon.TerraAddon;
+import com.dfsek.terra.api.addon.annotations.Addon;
+import com.dfsek.terra.api.addon.annotations.Author;
+import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.event.EventListener;
+import com.dfsek.terra.api.injection.annotations.Inject;
+
+@Addon("config-feature")
+@Version("1.0.0")
+@Author("Terra")
+public class FeatureAddon extends TerraAddon implements EventListener {
+    @Inject
+    private TerraPlugin main;
+
+    @Override
+    public void initialize() {
+        main.getEventManager().registerListener(this, this);
+    }
+}
