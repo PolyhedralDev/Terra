502d2b5814d79d7dda9ddac6bb8b6695f2bf218d
dfsek <dfsek@protonmail.com>
2021-07-21 20:08:23 -0700

    functional events in FloraAddon

--- common/addons/config-flora/src/main/java/com/dfsek/terra/addons/flora/FloraAddon.java
+++ common/addons/config-flora/src/main/java/com/dfsek/terra/addons/flora/FloraAddon.java
@@ -7,25 +7,23 @@ import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
 import com.dfsek.terra.api.addon.annotations.Version;
-import com.dfsek.terra.api.event.EventListener;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.injection.annotations.Inject;
-import com.dfsek.terra.api.registry.exception.DuplicateEntryException;
 
 @Addon("config-flora")
 @Author("Terra")
 @Version("0.1.0")
-public class FloraAddon extends TerraAddon implements EventListener {
+public class FloraAddon extends TerraAddon {
     @Inject
     private TerraPlugin main;
 
     @Override
     public void initialize() {
-        main.getEventManager().registerListener(this, this);
-    }
-
-    public void onPackLoad(ConfigPackPreLoadEvent event) throws DuplicateEntryException {
-        event.getPack().registerConfigType(new FloraConfigType(), "FLORA", 2);
-        event.getPack().applyLoader(BlockLayer.class, BlockLayerTemplate::new);
+        main.getEventManager()
+                .register(ConfigPackPreLoadEvent.class)
+                .then(event -> {
+                    event.getPack().registerConfigType(new FloraConfigType(), "FLORA", 2);
+                    event.getPack().applyLoader(BlockLayer.class, BlockLayerTemplate::new);
+                });
     }
 }
