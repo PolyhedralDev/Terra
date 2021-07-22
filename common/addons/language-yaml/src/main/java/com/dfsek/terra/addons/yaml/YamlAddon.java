185df6e8a4fbd2a8cc994d59fd1b7f5af16dee90
dfsek <dfsek@protonmail.com>
2021-07-21 20:17:45 -0700

    functional events in YamlAddon

--- common/addons/language-yaml/src/main/java/com/dfsek/terra/addons/yaml/YamlAddon.java
+++ common/addons/language-yaml/src/main/java/com/dfsek/terra/addons/yaml/YamlAddon.java
@@ -6,26 +6,23 @@ import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
 import com.dfsek.terra.api.addon.annotations.Version;
-import com.dfsek.terra.api.event.EventListener;
 import com.dfsek.terra.api.event.events.config.ConfigurationDiscoveryEvent;
 import com.dfsek.terra.api.injection.annotations.Inject;
 
 @Addon("language-yaml")
 @Version("1.0.0")
 @Author("Terra")
-public class YamlAddon extends TerraAddon implements EventListener {
+public class YamlAddon extends TerraAddon {
     @Inject
     private TerraPlugin main;
 
     @Override
     public void initialize() {
-        main.getEventManager().registerListener(this, this);
-    }
-
-    public void loadYamlConfigs(ConfigurationDiscoveryEvent event) {
-        event.getLoader().open("", ".yml").thenEntries(entries -> entries.forEach(entry -> {
-            main.getDebugLogger().info("Discovered config " + entry.getKey());
-            event.register(entry.getKey(), new YamlConfiguration(entry.getValue(), entry.getKey()));
-        }));
+        main.getEventManager()
+                .register(ConfigurationDiscoveryEvent.class)
+                .then(event -> event.getLoader().open("", ".yml").thenEntries(entries -> entries.forEach(entry -> {
+                    main.getDebugLogger().info("Discovered config " + entry.getKey());
+                    event.register(entry.getKey(), new YamlConfiguration(entry.getValue(), entry.getKey()));
+                })));
     }
 }
