bff0106e65e9846709ce923f1869fcedbff34b6f
dfsek <dfsek@protonmail.com>
2021-07-21 20:12:53 -0700

    functional events in PaletteAddon

--- common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
+++ common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
@@ -7,24 +7,23 @@ import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
 import com.dfsek.terra.api.addon.annotations.Version;
-import com.dfsek.terra.api.event.EventListener;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.injection.annotations.Inject;
 
 @Addon("config-palette")
 @Author("Terra")
 @Version("1.0.0")
-public class PaletteAddon extends TerraAddon implements EventListener {
+public class PaletteAddon extends TerraAddon {
     @Inject
     private TerraPlugin main;
 
     @Override
     public void initialize() {
-        main.getEventManager().registerListener(this, this);
-    }
-
-    public void onPackLoad(ConfigPackPreLoadEvent event) {
-        event.getPack().registerConfigType(new PaletteConfigType(main), "PALETTE", 2);
-        event.getPack().applyLoader(PaletteLayerHolder.class, new PaletteLayerLoader());
+        main.getEventManager()
+                .register(ConfigPackPreLoadEvent.class)
+                .then(event -> {
+                    event.getPack().registerConfigType(new PaletteConfigType(main), "PALETTE", 2);
+                    event.getPack().applyLoader(PaletteLayerHolder.class, new PaletteLayerLoader());
+                });
     }
 }
