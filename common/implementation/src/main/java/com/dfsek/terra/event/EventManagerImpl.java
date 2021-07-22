8eddffb0a6cdfb98d01a45d201ddb9d668e8aa06
dfsek <dfsek@protonmail.com>
2021-07-21 22:05:14 -0700

    delete EventListener

--- common/implementation/src/main/java/com/dfsek/terra/event/EventManagerImpl.java
+++ common/implementation/src/main/java/com/dfsek/terra/event/EventManagerImpl.java
@@ -1,20 +1,12 @@
 package com.dfsek.terra.event;
 
 import com.dfsek.terra.api.TerraPlugin;
-import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.event.EventHandler;
-import com.dfsek.terra.api.event.EventListener;
 import com.dfsek.terra.api.event.EventManager;
 import com.dfsek.terra.api.event.events.Event;
-import com.dfsek.terra.api.event.functional.EventContext;
 import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
-import com.dfsek.terra.api.util.reflection.TypeKey;
 
-import java.lang.reflect.Method;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class EventManagerImpl implements EventManager {
