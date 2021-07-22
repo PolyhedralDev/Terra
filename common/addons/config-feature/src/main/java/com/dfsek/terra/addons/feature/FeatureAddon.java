36ce88af0a30c78fa6a9d08df83cfaf0485505d8
dfsek <dfsek@protonmail.com>
2021-07-21 20:07:30 -0700

    functional events in FeatureAddon

--- common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
+++ common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
@@ -5,23 +5,20 @@ import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
 import com.dfsek.terra.api.addon.annotations.Version;
-import com.dfsek.terra.api.event.EventListener;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.injection.annotations.Inject;
 
 @Addon("config-feature")
 @Version("1.0.0")
 @Author("Terra")
-public class FeatureAddon extends TerraAddon implements EventListener {
+public class FeatureAddon extends TerraAddon {
     @Inject
     private TerraPlugin main;
 
     @Override
     public void initialize() {
-        main.getEventManager().registerListener(this, this);
-    }
-
-    public void onPackLoad(ConfigPackPreLoadEvent event) {
-        event.getPack().registerConfigType(new FeatureConfigType(), "FEATURE", 2);
+        main.getEventManager()
+                .register(ConfigPackPreLoadEvent.class)
+                .then(event -> event.getPack().registerConfigType(new FeatureConfigType(), "FEATURE", 2));
     }
 }
