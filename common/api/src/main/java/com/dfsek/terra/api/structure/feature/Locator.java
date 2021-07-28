26152ec94e8d8f19611d867f95cacdb50aab6d17
dfsek <dfsek@protonmail.com>
2021-07-28 08:58:10 -0700

    add boolean methods to Locator

--- common/api/src/main/java/com/dfsek/terra/api/structure/feature/Locator.java
+++ common/api/src/main/java/com/dfsek/terra/api/structure/feature/Locator.java
@@ -4,4 +4,12 @@ import com.dfsek.terra.api.world.Column;
 
 public interface Locator {
     BinaryColumn getSuitableCoordinates(Column column);
+
+    default Locator and(Locator that) {
+        return column -> this.getSuitableCoordinates(column).and(that.getSuitableCoordinates(column));
+    }
+
+    default Locator or(Locator that) {
+        return column -> this.getSuitableCoordinates(column).or(that.getSuitableCoordinates(column));
+    }
 }
