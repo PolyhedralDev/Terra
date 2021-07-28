fb3f90a9cde57e879136f5d83bd4fe7ee8a646e2
dfsek <dfsek@protonmail.com>
2021-07-27 19:39:53 -0700

    Pattern boolean operations

--- common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/patterns/Pattern.java
+++ common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/patterns/Pattern.java
@@ -4,4 +4,12 @@ import com.dfsek.terra.api.world.Column;
 
 public interface Pattern {
     boolean matches(int y, Column column);
+
+    default Pattern and(Pattern that) {
+        return (y, column) -> this.matches(y, column) && that.matches(y, column);
+    }
+
+    default Pattern or(Pattern that) {
+        return (y, column) -> this.matches(y, column) || that.matches(y, column);
+    }
 }
