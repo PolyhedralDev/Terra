ec19c468a27f972627adfeceb76a2c319733fa07
dfsek <dfsek@protonmail.com>
2021-07-27 20:13:35 -0700

    meta annotate distributors

--- common/addons/config-distributors/src/main/java/com/dfsek/terra/addons/feature/distributor/config/OrDistributorTemplate.java
+++ common/addons/config-distributors/src/main/java/com/dfsek/terra/addons/feature/distributor/config/OrDistributorTemplate.java
@@ -4,13 +4,14 @@ import com.dfsek.tectonic.annotations.Value;
 import com.dfsek.tectonic.config.ValidatedConfigTemplate;
 import com.dfsek.tectonic.exception.ValidationException;
 import com.dfsek.tectonic.loading.object.ObjectTemplate;
+import com.dfsek.terra.api.config.meta.Meta;
 import com.dfsek.terra.api.structure.feature.Distributor;
 
 import java.util.List;
 
 public class OrDistributorTemplate implements ObjectTemplate<Distributor>, ValidatedConfigTemplate {
     @Value("distributors")
-    private List<Distributor> distributors;
+    private @Meta List<@Meta Distributor> distributors;
 
 
     @Override
