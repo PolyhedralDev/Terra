2263e6722bd9403c287cfac0f42cb889d4091de3
dfsek <dfsek@protonmail.com>
2021-07-30 10:53:22 -0700

    set up mixin and vanillagradle

--- platforms/sponge/build.gradle.kts
+++ platforms/sponge/build.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("org.spongepowered.plugin").version("0.9.0")
+    id("org.spongepowered.gradle.vanilla").version("0.2")
+}
+
 repositories {
     maven {
         url = uri("https://repo-new.spongepowered.org/repository/maven-public/")
@@ -6,5 +11,28 @@ repositories {
 
 dependencies {
     "shadedApi"(project(":common:implementation"))
-    annotationProcessor(implementation("org.spongepowered:spongeapi:8.0.0-SNAPSHOT")!!)
+    annotationProcessor(implementation("org.spongepowered:spongeapi:9.0.0-SNAPSHOT")!!)
+    annotationProcessor("org.spongepowered:mixin:0.8.2:processor")
+}
+
+sponge {
+    plugin {
+        id = "terra"
+    }
+}
+
+minecraft {
+    version("1.17.1")
+    runs {
+        server()
+        client()
+    }
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes(
+                mapOf("MixinConfigs" to "terra.mixins.json")
+        )
+    }
 }
