03241fa41284641a0ed6a18266880a4225f41329
dfsek <dfsek@protonmail.com>
2021-07-31 00:56:45 -0700

    register event listeners

--- platforms/sponge/src/main/java/com/dfsek/terra/sponge/TerraSpongePlugin.java
+++ platforms/sponge/src/main/java/com/dfsek/terra/sponge/TerraSpongePlugin.java
@@ -1,6 +1,7 @@
 package com.dfsek.terra.sponge;
 
 import com.google.inject.Inject;
+import org.spongepowered.api.Game;
 import org.spongepowered.plugin.PluginContainer;
 import org.spongepowered.plugin.jvm.Plugin;
 
@@ -9,9 +10,10 @@ public class TerraSpongePlugin {
     private final PluginContainer plugin;
     private final TerraPluginImpl terraPlugin;
     @Inject
-    public TerraSpongePlugin(PluginContainer plugin) {
+    public TerraSpongePlugin(PluginContainer plugin, Game game) {
         this.plugin = plugin;
         this.terraPlugin = new TerraPluginImpl(this);
+        game.eventManager().registerListeners(plugin, new SpongeListener(this));
     }
 
     public PluginContainer getPluginContainer() {
