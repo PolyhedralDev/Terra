016fbaf46804f5ccba1f69870ffceab10c6128a4
dfsek <dfsek@protonmail.com>
2021-09-24 07:54:57 -0700

    create config api subproject

---
+++ common/api/config/build.gradle.kts
@@ -0,0 +1,10 @@
+dependencies {
+    "shadedApi"(project(":common:api:core"))
+    
+    "shadedApi"("com.dfsek:Paralithic:0.5.0")
+    
+    "shadedApi"("com.dfsek.tectonic:common:2.1.2")
+    
+    "shadedApi"("net.jafama:jafama:2.3.2")
+}
+
