c928a1a8067f26e8fea50b66db927dfa9b36b2e1
dfsek <dfsek@protonmail.com>
2021-09-24 22:06:26 -0700

    more Column API

--- common/addons/generation-stage-feature/src/main/java/com/dfsek/terra/addons/generation/feature/ColumnImpl.java
+++ common/addons/generation-stage-feature/src/main/java/com/dfsek/terra/addons/generation/feature/ColumnImpl.java
@@ -1,8 +1,11 @@
 package com.dfsek.terra.addons.generation.feature;
 
 import com.dfsek.terra.api.block.state.BlockState;
+import com.dfsek.terra.api.structure.feature.BinaryColumn;
 import com.dfsek.terra.api.world.Column;
 import com.dfsek.terra.api.world.World;
+
+import java.util.function.IntConsumer;
 
 
 public class ColumnImpl implements Column {
@@ -44,5 +47,17 @@ public class ColumnImpl implements Column {
     @Override
     public int getMaxY() {
         return world.getMaxHeight();
+    }
+    
+    @Override
+    public void forEach(IntConsumer function) {
+        for(int y = world.getMinHeight(); y < world.getMaxHeight(); y++) {
+            function.accept(y);
+        }
+    }
+    
+    @Override
+    public BinaryColumn newBinaryColumn() {
+        return new BinaryColumn(getMinY(), getMaxY());
     }
 }
