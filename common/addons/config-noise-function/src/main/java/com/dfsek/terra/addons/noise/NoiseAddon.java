3d141fc47f623ad55010beb9ffdc0c1393bb3440
dfsek <dfsek@protonmail.com>
2021-11-09 23:49:49 -0700

    move registry api to new module

--- common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/NoiseAddon.java
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/NoiseAddon.java
@@ -2,7 +2,6 @@ package com.dfsek.terra.addons.noise;
 
 import com.dfsek.tectonic.loading.object.ObjectTemplate;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Supplier;
