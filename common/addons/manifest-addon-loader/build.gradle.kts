c6e6478043bf8643483f5817e7ae4700f388c721
dfsek <dfsek@protonmail.com>
2021-11-17 08:52:23 -0700

    load bootstrap addons

--- common/addons/manifest-addon-loader/build.gradle.kts
+++ common/addons/manifest-addon-loader/build.gradle.kts
@@ -2,3 +2,9 @@ dependencies {
     "shadedApi"("commons-io:commons-io:2.6")
     "shadedImplementation"("com.dfsek.tectonic:yaml:2.1.2")
 }
+
+tasks.withType<Jar>() {
+    manifest {
+        attributes("Bootstrap-Addon-Entry-Point" to "com.dfsek.terra.addons.manifest.impl.ManifestAddonLoader")
+    }
+}
