4ba759c3743641fa1567da05ab89ae70093f9287
dfsek <dfsek@protonmail.com>
2021-11-17 12:39:24 -0700

    create addon manifests

--- common/addons/config-ore/src/main/java/com/dfsek/terra/addons/ore/OreAddon.java
+++ common/addons/config-ore/src/main/java/com/dfsek/terra/addons/ore/OreAddon.java
@@ -1,6 +1,8 @@
 package com.dfsek.terra.addons.ore;
 
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
 import com.dfsek.terra.api.Platform;
+import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
@@ -11,18 +13,19 @@ import com.dfsek.terra.api.inject.annotations.Inject;
 import com.dfsek.terra.api.world.generator.GenerationStageProvider;
 
 
-@Addon("config-ore")
-@Author("Terra")
-@Version("1.0.0")
-public class OreAddon extends TerraAddon {
+
+public class OreAddon implements AddonInitializer {
     @Inject
     private Platform platform;
+    
+    @Inject
+    private BaseAddon addon;
     
     @Override
     public void initialize() {
         platform.getEventManager()
                 .getHandler(FunctionalEventHandler.class)
-                .register(this, ConfigPackPreLoadEvent.class)
+                .register(addon, ConfigPackPreLoadEvent.class)
                 .then(event -> {
                 event.getPack().registerConfigType(new OreConfigType(), "ORE", 1);
                 event.getPack().getOrCreateRegistry(GenerationStageProvider.class).register("ORE", pack -> new OrePopulator(platform));
