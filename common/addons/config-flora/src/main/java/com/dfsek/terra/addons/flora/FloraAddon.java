dbfd354338312763fe599cbaf35328f61326f00b
dfsek <dfsek@protonmail.com>
2021-11-17 12:39:24 -0700

    create addon manifests

--- common/addons/config-flora/src/main/java/com/dfsek/terra/addons/flora/FloraAddon.java
+++ common/addons/config-flora/src/main/java/com/dfsek/terra/addons/flora/FloraAddon.java
@@ -2,28 +2,27 @@ package com.dfsek.terra.addons.flora;
 
 import com.dfsek.terra.addons.flora.config.BlockLayerTemplate;
 import com.dfsek.terra.addons.flora.flora.gen.BlockLayer;
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
 import com.dfsek.terra.api.Platform;
-import com.dfsek.terra.api.addon.TerraAddon;
-import com.dfsek.terra.api.addon.annotations.Addon;
-import com.dfsek.terra.api.addon.annotations.Author;
-import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
 import com.dfsek.terra.api.inject.annotations.Inject;
 
 
-@Addon("config-flora")
-@Author("Terra")
-@Version("0.1.0")
-public class FloraAddon extends TerraAddon {
+
+public class FloraAddon implements AddonInitializer {
     @Inject
     private Platform platform;
+    
+    @Inject
+    private BaseAddon addon;
     
     @Override
     public void initialize() {
         platform.getEventManager()
                 .getHandler(FunctionalEventHandler.class)
-                .register(this, ConfigPackPreLoadEvent.class)
+                .register(addon, ConfigPackPreLoadEvent.class)
                 .then(event -> {
                 event.getPack().registerConfigType(new FloraConfigType(), "FLORA", 2);
                 event.getPack().applyLoader(BlockLayer.class, BlockLayerTemplate::new);
