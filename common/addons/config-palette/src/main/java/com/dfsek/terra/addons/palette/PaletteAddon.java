c406bfd63f35290abc5dca7c800a0cb84c706c26
dfsek <dfsek@protonmail.com>
2021-11-17 16:28:40 -0700

    format

--- common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
+++ common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
@@ -5,14 +5,9 @@ import com.dfsek.terra.addons.palette.palette.PaletteLayerHolder;
 import com.dfsek.terra.addons.palette.palette.PaletteLayerLoader;
 import com.dfsek.terra.api.Platform;
 import com.dfsek.terra.api.addon.BaseAddon;
-import com.dfsek.terra.api.addon.TerraAddon;
-import com.dfsek.terra.api.addon.annotations.Addon;
-import com.dfsek.terra.api.addon.annotations.Author;
-import com.dfsek.terra.api.addon.annotations.Version;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
 import com.dfsek.terra.api.inject.annotations.Inject;
-
 
 
 public class PaletteAddon implements AddonInitializer {
@@ -28,9 +23,9 @@ public class PaletteAddon implements AddonInitializer {
                 .getHandler(FunctionalEventHandler.class)
                 .register(addon, ConfigPackPreLoadEvent.class)
                 .then(event -> {
-                event.getPack().registerConfigType(new PaletteConfigType(platform), "PALETTE", 2);
-                event.getPack().applyLoader(PaletteLayerHolder.class, new PaletteLayerLoader());
-            })
+                    event.getPack().registerConfigType(new PaletteConfigType(platform), "PALETTE", 2);
+                    event.getPack().applyLoader(PaletteLayerHolder.class, new PaletteLayerLoader());
+                })
                 .failThrough();
     }
 }
