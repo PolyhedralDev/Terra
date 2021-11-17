26e5e4c0c8918f7dc227856e91cc7c7c16eb6b50
dfsek <dfsek@protonmail.com>
2021-11-17 12:39:24 -0700

    create addon manifests

--- common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
+++ common/addons/config-feature/src/main/java/com/dfsek/terra/addons/feature/FeatureAddon.java
@@ -1,27 +1,25 @@
 package com.dfsek.terra.addons.feature;
 
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
 import com.dfsek.terra.api.Platform;
-import com.dfsek.terra.api.addon.TerraAddon;
-import com.dfsek.terra.api.addon.annotations.Addon;
-import com.dfsek.terra.api.addon.annotations.Author;
-import com.dfsek.terra.api.addon.annotations.Version;
+import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.event.events.config.pack.ConfigPackPreLoadEvent;
 import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
 import com.dfsek.terra.api.inject.annotations.Inject;
 
 
-@Addon("config-feature")
-@Version("1.0.0")
-@Author("Terra")
-public class FeatureAddon extends TerraAddon {
+public class FeatureAddon implements AddonInitializer {
     @Inject
     private Platform platform;
+    
+    @Inject
+    private BaseAddon addon;
     
     @Override
     public void initialize() {
         platform.getEventManager()
                 .getHandler(FunctionalEventHandler.class)
-                .register(this, ConfigPackPreLoadEvent.class)
+                .register(addon, ConfigPackPreLoadEvent.class)
                 .then(event -> event.getPack().registerConfigType(new FeatureConfigType(), "FEATURE", 3))
                 .failThrough();
     }
