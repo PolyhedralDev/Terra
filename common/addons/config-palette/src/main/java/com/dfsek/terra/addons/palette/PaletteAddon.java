eb9f484863a4468aa641e81ac8675b362daec43e
dfsek <dfsek@protonmail.com>
2021-11-17 12:39:24 -0700

    create addon manifests

--- common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
+++ common/addons/config-palette/src/main/java/com/dfsek/terra/addons/palette/PaletteAddon.java
@@ -1,8 +1,10 @@
 package com.dfsek.terra.addons.palette;
 
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
 import com.dfsek.terra.addons.palette.palette.PaletteLayerHolder;
 import com.dfsek.terra.addons.palette.palette.PaletteLayerLoader;
 import com.dfsek.terra.api.Platform;
+import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
@@ -12,18 +14,19 @@ import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
 import com.dfsek.terra.api.inject.annotations.Inject;
 
 
-@Addon("config-palette")
-@Author("Terra")
-@Version("1.0.0")
-public class PaletteAddon extends TerraAddon {
+
+public class PaletteAddon implements AddonInitializer {
     @Inject
     private Platform platform;
+    
+    @Inject
+    private BaseAddon addon;
     
     @Override
     public void initialize() {
         platform.getEventManager()
                 .getHandler(FunctionalEventHandler.class)
-                .register(this, ConfigPackPreLoadEvent.class)
+                .register(addon, ConfigPackPreLoadEvent.class)
                 .then(event -> {
                 event.getPack().registerConfigType(new PaletteConfigType(platform), "PALETTE", 2);
                 event.getPack().applyLoader(PaletteLayerHolder.class, new PaletteLayerLoader());
