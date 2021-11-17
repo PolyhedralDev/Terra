a2a3dbbc8753aeb00dbe29a2d8ced5a047bfa3c4
dfsek <dfsek@protonmail.com>
2021-11-17 12:39:24 -0700

    create addon manifests

--- common/addons/language-yaml/src/main/java/com/dfsek/terra/addons/yaml/YamlAddon.java
+++ common/addons/language-yaml/src/main/java/com/dfsek/terra/addons/yaml/YamlAddon.java
@@ -2,7 +2,9 @@ package com.dfsek.terra.addons.yaml;
 
 import com.dfsek.tectonic.yaml.YamlConfiguration;
 
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
 import com.dfsek.terra.api.Platform;
+import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.addon.TerraAddon;
 import com.dfsek.terra.api.addon.annotations.Addon;
 import com.dfsek.terra.api.addon.annotations.Author;
@@ -12,18 +14,18 @@ import com.dfsek.terra.api.event.functional.FunctionalEventHandler;
 import com.dfsek.terra.api.inject.annotations.Inject;
 
 
-@Addon("language-yaml")
-@Version("1.0.0")
-@Author("Terra")
-public class YamlAddon extends TerraAddon {
+public class YamlAddon implements AddonInitializer {
     @Inject
     private Platform platform;
+    
+    @Inject
+    private BaseAddon addon;
     
     @Override
     public void initialize() {
         platform.getEventManager()
                 .getHandler(FunctionalEventHandler.class)
-                .register(this, ConfigurationDiscoveryEvent.class)
+                .register(addon, ConfigurationDiscoveryEvent.class)
                 .then(event -> event.getLoader().open("", ".yml").thenEntries(entries -> entries.forEach(entry -> {
                 platform.getDebugLogger().info("Discovered config " + entry.getKey());
                 event.register(entry.getKey(), new YamlConfiguration(entry.getValue(), entry.getKey()));
