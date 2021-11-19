a6440187ed8cfd7cd1fa594b6559d30e53f3b40d
dfsek <dfsek@protonmail.com>
2021-11-18 21:13:32 -0700

    addon dependency sorting

--- common/addons/api-addon-loader/src/main/java/com/dfsek/terra/addon/loader/ApiAddonLoader.java
+++ common/addons/api-addon-loader/src/main/java/com/dfsek/terra/addon/loader/ApiAddonLoader.java
@@ -3,11 +3,15 @@ package com.dfsek.terra.addon.loader;
 import java.nio.file.Path;
 import java.util.Collections;
 
+import ca.solostudios.strata.Versions;
+import ca.solostudios.strata.version.Version;
+
 import com.dfsek.terra.api.addon.BaseAddon;
 import com.dfsek.terra.api.addon.bootstrap.BootstrapBaseAddon;
 
 
 public class ApiAddonLoader implements BootstrapBaseAddon<BaseAddon> {
+    private static final Version VERSION = Versions.getVersion(1, 0, 0);
     @Override
     public Iterable<BaseAddon> loadAddons(Path addonsFolder, ClassLoader parent) {
         return Collections.emptySet();
@@ -16,5 +20,10 @@ public class ApiAddonLoader implements BootstrapBaseAddon<BaseAddon> {
     @Override
     public String getID() {
         return "API";
+    }
+    
+    @Override
+    public Version getVersion() {
+        return VERSION;
     }
 }
