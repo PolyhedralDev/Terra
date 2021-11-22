45c682c59536c6437565d33c429b4abc7e115466
dfsek <dfsek@protonmail.com>
2021-11-22 09:03:02 -0700

    fix mixin remap issue

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/mixin/implementations/block/BlockMixin.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/mixin/implementations/block/BlockMixin.java
@@ -18,6 +18,7 @@
 package com.dfsek.terra.fabric.mixin.implementations.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import org.spongepowered.asm.mixin.Implements;
 import org.spongepowered.asm.mixin.Interface;
@@ -26,7 +27,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
 import com.dfsek.terra.api.block.BlockType;
-import com.dfsek.terra.api.block.state.BlockState;
 import com.dfsek.terra.fabric.util.FabricAdapter;
 
 
@@ -34,14 +34,14 @@ import com.dfsek.terra.fabric.util.FabricAdapter;
 @Implements(@Interface(iface = BlockType.class, prefix = "terra$", remap = Interface.Remap.NONE))
 public abstract class BlockMixin {
     @Shadow
-    private net.minecraft.block.BlockState defaultState;
+    private BlockState defaultState;
     
     @Intrinsic
     public Object terra$getHandle() {
         return this;
     }
     
-    public BlockState terra$getDefaultData() {
+    public com.dfsek.terra.api.block.state.BlockState terra$getDefaultData() {
         return FabricAdapter.adapt(defaultState);
     }
     
