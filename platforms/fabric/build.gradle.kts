596262bd6454206ce60b8e7454e6a009d6ded42b
dfsek <dfsek@protonmail.com>
2021-11-23 12:43:05 -0700

    pre7

--- platforms/fabric/build.gradle.kts
+++ platforms/fabric/build.gradle.kts
@@ -16,7 +16,7 @@ tasks.named<ShadowJar>("shadowJar") {
     relocate("org.yaml", "com.dfsek.terra.lib.yaml")
 }
 
-val minecraft = "1.18-pre6"
+val minecraft = "1.18-pre7"
 val yarn = "3"
 val fabricLoader = "0.12.5"
 
