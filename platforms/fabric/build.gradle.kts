6c0628ddcbdd831092a2c048025ab7ce981f57c4
dfsek <dfsek@protonmail.com>
2021-11-25 09:38:41 -0700

    reorganize Fabric buildscript

--- platforms/fabric/build.gradle.kts
+++ platforms/fabric/build.gradle.kts
@@ -3,17 +3,14 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.modrinth.minotaur.TaskModrinthUpload
 import net.fabricmc.loom.task.RemapJarTask
 
+val minecraft = "1.18-pre8"
+val yarn = "4"
+val fabricLoader = "0.12.5"
+
 plugins {
     id("fabric-loom").version("0.10.58")
     id("com.modrinth.minotaur").version("1.1.0")
 }
-
-addonDir(project.rootProject.file("./run/config/Terra/addons"), tasks.named("runClient").get())
-addonDir(project.rootProject.file("./run/config/Terra/addons"), tasks.named("runServer").get())
-
-val minecraft = "1.18-pre8"
-val yarn = "4"
-val fabricLoader = "0.12.5"
 
 // Do not shade because minecraft already includes it
 dependencies {
@@ -34,15 +31,19 @@ dependencies {
     modImplementation("net.fabricmc:fabric-loader:$fabricLoader")
 }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(17)
-}
-
 loom {
     accessWidenerPath.set(file("src/main/resources/terra.accesswidener"))
     mixin {
         defaultRefmapName.set("terra-refmap.json")
     }
+}
+
+
+addonDir(project.rootProject.file("./run/config/Terra/addons"), tasks.named("runClient").get())
+addonDir(project.rootProject.file("./run/config/Terra/addons"), tasks.named("runServer").get())
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(17)
 }
 
 val remapped = tasks.register<RemapJarTask>("remapShadedJar") {
