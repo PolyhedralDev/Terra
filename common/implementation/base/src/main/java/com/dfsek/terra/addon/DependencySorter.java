7376533027194163bdca6837d2c5a719ef2c91a0
dfsek <dfsek@protonmail.com>
2021-11-27 09:58:01 -0700

    dont check addon versions until Strata is fixed

--- common/implementation/base/src/main/java/com/dfsek/terra/addon/DependencySorter.java
+++ common/implementation/base/src/main/java/com/dfsek/terra/addon/DependencySorter.java
@@ -50,8 +50,9 @@ public class DependencySorter {
             BaseAddon dependency = addons.get(id);
             
             if(!range.isSatisfiedBy(dependency.getVersion())) {
-                throw new DependencyVersionException("Addon " + addon.getID() + " specifies dependency on " + id + ", versions " + range +
-                                                     ", but non-matching version " + dependency.getVersion().getFormatted() + " is installed..");
+                // FIXME - Strata is currently broken.
+                //throw new DependencyVersionException("Addon " + addon.getID() + " specifies dependency on " + id + ", versions " + range +
+                //                                     ", but non-matching version " + dependency.getVersion().getFormatted() + " is installed..");
             }
             
             if(!visited.get(dependency.getID())) { // if we've not visited it yet
