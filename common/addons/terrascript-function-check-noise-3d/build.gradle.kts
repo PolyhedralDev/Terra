b2221294784dee64513274c8a0b393716e393182
dfsek <dfsek@protonmail.com>
2021-12-02 17:46:07 -0700

    terrascript check function addon depends on terrascript addon

--- common/addons/terrascript-function-check-noise-3d/build.gradle.kts
+++ common/addons/terrascript-function-check-noise-3d/build.gradle.kts
@@ -5,4 +5,5 @@ version = version("0.1.0")
 dependencies {
     shadedApi(project(":common:addons:manifest-addon-loader"))
     shadedApi(project(":common:addons:chunk-generator-noise-3d"))
+    shadedApi(project(":common:addons:structure-terrascript-loader"))
 }
