4d2639207db78acc29679136e1d520d2b644ad35
dfsek <dfsek@protonmail.com>
2021-12-03 15:55:56 -0700

    fix terrascript loading on windows

--- common/api/util/src/main/java/com/dfsek/terra/api/util/StringUtil.java
+++ common/api/util/src/main/java/com/dfsek/terra/api/util/StringUtil.java
@@ -14,6 +14,8 @@ public class StringUtil {
     public static String fileName(String path) {
         if(path.contains(File.separator)) {
             return path.substring(path.lastIndexOf(File.separatorChar) + 1, path.lastIndexOf('.'));
+        } else if(path.contains("/")) {
+            return path.substring(path.lastIndexOf("/") + 1, path.lastIndexOf('.'));
         } else if(path.contains(".")) {
             return path.substring(0, path.lastIndexOf('.'));
         } else {
