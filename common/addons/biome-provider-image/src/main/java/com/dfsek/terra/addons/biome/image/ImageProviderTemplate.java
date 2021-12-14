e39730c238e01bc2b81795b4438238cabf49d5fb
dfsek <dfsek@protonmail.com>
2021-12-14 13:45:33 -0700

    document ImageProviderTemplate

--- common/addons/biome-provider-image/src/main/java/com/dfsek/terra/addons/biome/image/ImageProviderTemplate.java
+++ common/addons/biome-provider-image/src/main/java/com/dfsek/terra/addons/biome/image/ImageProviderTemplate.java
@@ -8,6 +8,7 @@
 package com.dfsek.terra.addons.biome.image;
 
 import com.dfsek.tectonic.api.config.template.annotations.Default;
+import com.dfsek.tectonic.api.config.template.annotations.Description;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
 import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
 
@@ -24,10 +25,13 @@ public class ImageProviderTemplate implements ObjectTemplate<BiomeProvider> {
     private final Registry<Biome> biomes;
     @Value("resolution")
     @Default
+    @Description("Sets the resolution at which to sample the image.")
     private int resolution = 1;
     @Value("image.name")
+    @Description("Sets the location of the image on the filesystem, relative to the pack root.")
     private BufferedImage image;
     @Value("image.align")
+    @Description("Sets the alignment style to use for the image.")
     private ImageBiomeProvider.Align align;
     
     public ImageProviderTemplate(Registry<Biome> set) {
