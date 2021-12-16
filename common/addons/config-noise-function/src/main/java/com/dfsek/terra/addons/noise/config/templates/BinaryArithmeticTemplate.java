b73cbdd8a2470e4957e84e2b29566b28872eff64
dfsek <dfsek@protonmail.com>
2021-12-16 09:43:18 -0700

    add meta annotations to BinaryArithmeticTemplate

--- common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/config/templates/BinaryArithmeticTemplate.java
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/config/templates/BinaryArithmeticTemplate.java
@@ -3,6 +3,7 @@ package com.dfsek.terra.addons.noise.config.templates;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
 
 import com.dfsek.terra.addons.noise.samplers.arithmetic.BinaryArithmeticSampler;
+import com.dfsek.terra.api.config.meta.Meta;
 import com.dfsek.terra.api.noise.NoiseSampler;
 
 import java.util.function.BiFunction;
@@ -11,9 +12,9 @@ import java.util.function.BiFunction;
 public class BinaryArithmeticTemplate<T extends BinaryArithmeticSampler> extends SamplerTemplate<T> {
     private final BiFunction<NoiseSampler, NoiseSampler, T> function;
     @Value("left")
-    private NoiseSampler left;
+    private @Meta NoiseSampler left;
     @Value("right")
-    private NoiseSampler right;
+    private @Meta NoiseSampler right;
     
     public BinaryArithmeticTemplate(BiFunction<NoiseSampler, NoiseSampler, T> function) {
         this.function = function;
