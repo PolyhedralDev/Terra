33cdc55e21359d8adad02a92d13573623e411733
dfsek <dfsek@protonmail.com>
2021-12-15 20:49:10 -0700

    create and register ScaleNormalizerTemplate

--- common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/NoiseAddon.java
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/NoiseAddon.java
@@ -32,6 +32,7 @@ import com.dfsek.terra.addons.noise.config.templates.normalizer.ClampNormalizerT
 import com.dfsek.terra.addons.noise.config.templates.normalizer.LinearNormalizerTemplate;
 import com.dfsek.terra.addons.noise.config.templates.normalizer.NormalNormalizerTemplate;
 import com.dfsek.terra.addons.noise.config.templates.normalizer.ProbabilityNormalizerTemplate;
+import com.dfsek.terra.addons.noise.config.templates.normalizer.ScaleNormalizerTemplate;
 import com.dfsek.terra.addons.noise.samplers.noise.CellularSampler;
 import com.dfsek.terra.addons.noise.samplers.noise.random.GaussianNoiseSampler;
 import com.dfsek.terra.addons.noise.samplers.noise.random.PositiveWhiteNoiseSampler;
@@ -80,6 +81,7 @@ public class NoiseAddon implements AddonInitializer {
                   noiseRegistry.register("NORMAL", NormalNormalizerTemplate::new);
                   noiseRegistry.register("CLAMP", ClampNormalizerTemplate::new);
                   noiseRegistry.register("PROBABILITY", ProbabilityNormalizerTemplate::new);
+                  noiseRegistry.register("SCALE", ScaleNormalizerTemplate::new);
             
                   noiseRegistry.register("IMAGE", ImageSamplerTemplate::new);
             
---
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/config/templates/normalizer/ScaleNormalizerTemplate.java
@@ -0,0 +1,17 @@
+package com.dfsek.terra.addons.noise.config.templates.normalizer;
+
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+
+import com.dfsek.terra.addons.noise.normalizer.ScaleNormalizer;
+import com.dfsek.terra.api.noise.NoiseSampler;
+
+
+public class ScaleNormalizerTemplate extends NormalizerTemplate<ScaleNormalizer> {
+    @Value("amplitude")
+    private double amplitude;
+    
+    @Override
+    public NoiseSampler get() {
+        return new ScaleNormalizer(function, amplitude);
+    }
+}
