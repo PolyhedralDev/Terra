907c1519bde10eb7951916e1ff4c3f7b7257ea37
dfsek <dfsek@protonmail.com>
2021-12-15 20:46:08 -0700

    create MinSampler

---
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/samplers/arithmetic/MinSampler.java
@@ -0,0 +1,17 @@
+package com.dfsek.terra.addons.noise.samplers.arithmetic;
+
+import com.dfsek.terra.api.noise.NoiseSampler;
+
+import net.jafama.FastMath;
+
+
+public class MinSampler extends BinaryArithmeticSampler{
+    protected MinSampler(NoiseSampler left, NoiseSampler right) {
+        super(left, right);
+    }
+    
+    @Override
+    public double operate(double left, double right) {
+        return FastMath.min(left, right);
+    }
+}
