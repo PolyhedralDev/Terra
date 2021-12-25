bb87bfa1de7159a6f1ef0ff74175bf581f8d6404
dfsek <dfsek@protonmail.com>
2021-12-24 22:59:52 -0700

    implement Namespaced in BaseAddon

--- common/api/src/main/java/com/dfsek/terra/api/addon/BaseAddon.java
+++ common/api/src/main/java/com/dfsek/terra/api/addon/BaseAddon.java
@@ -13,13 +13,14 @@ import ca.solostudios.strata.version.VersionRange;
 import java.util.Collections;
 import java.util.Map;
 
+import com.dfsek.terra.api.registry.key.Namespaced;
 import com.dfsek.terra.api.registry.key.StringIdentifiable;
 
 
 /**
  * Base interface which all Terra addons extend
  */
-public interface BaseAddon extends StringIdentifiable {
+public interface BaseAddon extends StringIdentifiable, Namespaced {
     /**
      * Initializes the addon. To be implemented by addons, but never manually invoked.
      */
@@ -38,4 +39,8 @@ public interface BaseAddon extends StringIdentifiable {
      * @return Version of addon
      */
     Version getVersion();
+    
+    default String getNamespace() {
+        return getID();
+    }
 }
