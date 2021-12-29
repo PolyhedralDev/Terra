70b0c55a39954f478064c7c885ce8eee72e05be9
dfsek <dfsek@protonmail.com>
2021-12-28 20:14:42 -0700

    create command-structures addon

---
+++ common/addons/command-structures/src/main/java/com/dfsek/terra/addons/commands/structure/StructureCommandAddon.java
@@ -0,0 +1,11 @@
+package com.dfsek.terra.addons.commands.structure;
+
+import com.dfsek.terra.addons.manifest.api.AddonInitializer;
+
+
+public class StructureCommandAddon implements AddonInitializer {
+    @Override
+    public void initialize() {
+    
+    }
+}
