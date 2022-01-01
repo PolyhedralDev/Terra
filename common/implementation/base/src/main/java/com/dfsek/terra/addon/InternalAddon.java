17470fd74beb95c99561777b89f9c994b285aeb7
dfsek <dfsek@protonmail.com>
2021-12-31 18:12:56 -0700

    create PacksCommandAddon

--- common/implementation/base/src/main/java/com/dfsek/terra/addon/InternalAddon.java
+++ common/implementation/base/src/main/java/com/dfsek/terra/addon/InternalAddon.java
@@ -79,55 +79,6 @@ public class InternalAddon implements BaseAddon {
                                           })
                                    )
                            .command(
-                                   manager.commandBuilder("packs", ArgumentDescription.of("List installed config packs"))
-                                          .permission("terra.packs")
-                                          .handler(context -> {
-                                              StringBuilder packs = new StringBuilder("Installed packs:\n");
-                                              platform.getConfigRegistry().forEach(pack -> packs.append(" - ")
-                                                                                                .append(pack.getID())
-                                                                                                .append('@')
-                                                                                                .append(pack.getVersion().getFormatted()));
-                                              context.getSender().sendMessage(packs.toString());
-                                          })
-                                   )
-                           .command(
-                                   manager.commandBuilder("packs")
-                                          .literal("info", ArgumentDescription.of("Get information about a pack"))
-                                          .permission("terra.packs.info")
-                                          .argument(RegistryArgument.of("pack", platform.getConfigRegistry()))
-                                          .handler(context -> {
-                                              ConfigPack pack = context.get("pack");
-                                              StringBuilder packInfo = new StringBuilder("Pack ").append(pack.getID()).append('\n');
-                        
-                                              packInfo.append("Version: ").append(pack.getVersion().getFormatted()).append('\n');
-                                              packInfo.append("Author: ").append(pack.getAuthor()).append('\n');
-                        
-                                              packInfo.append("Addon Dependencies:\n");
-                                              pack.addons().forEach((id, versions) -> packInfo
-                                                      .append(" - ")
-                                                      .append(id.getID())
-                                                      .append('@')
-                                                      .append(versions.getFormatted())
-                                                      .append('\n'));
-                                              context.getSender().sendMessage(packInfo.toString());
-                                          }))
-                           .command(
-                                   manager.commandBuilder("packs")
-                                          .literal("reload", ArgumentDescription.of("Reload config packs"))
-                                          .permission("terra.packs.reload")
-                                          .handler(context -> {
-                                              context.getSender().sendMessage("Reloading Terra...");
-                                              logger.info("Reloading Terra...");
-                                              if(platform.reload()) {
-                                                  logger.info("Terra reloaded successfully.");
-                                                  context.getSender().sendMessage("Terra reloaded successfully.");
-                                              } else {
-                                                  logger.error("Terra failed to reload.");
-                                                  context.getSender().sendMessage(
-                                                          "Terra failed to reload. See logs for more information.");
-                                              }
-                                          }))
-                           .command(
                                    manager.commandBuilder("profiler", ArgumentDescription.of("Access the profiler"))
                                           .literal("start", ArgumentDescription.of("Start profiling"), "st")
                                           .permission("terra.profiler.start")
