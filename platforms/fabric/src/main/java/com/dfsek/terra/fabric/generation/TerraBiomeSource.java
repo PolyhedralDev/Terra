69a5cfecd1d8248ee1ec3d63f9ab8716fb86b6a1
dfsek <dfsek@protonmail.com>
2022-05-25 17:51:32 -0700

    remove println

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/generation/TerraBiomeSource.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/generation/TerraBiomeSource.java
@@ -43,9 +43,6 @@ public class TerraBiomeSource extends BiomeSource {
                                   .getBiomes()
                                   .spliterator(), false)
                       .map(b -> ((ProtoPlatformBiome) b.getPlatformBiome()).getDelegate()));
-        biomes.streamEntries().forEach(biomeReference -> {
-            System.out.println(biomeReference.getKey().orElseThrow());
-        });
         /*
         A little (unfortunately, required) jank to watch out for:
         
@@ -73,17 +70,6 @@ public class TerraBiomeSource extends BiomeSource {
     
     @Override
     public RegistryEntry<net.minecraft.world.biome.Biome> getBiome(int biomeX, int biomeY, int biomeZ, MultiNoiseSampler noiseSampler) {
-        /*return biomeRegistry
-                .getEntry(((ProtoPlatformBiome) pack
-                        .getBiomeProvider()
-                        .getBiome(biomeX << 2, biomeZ << 2, seed)
-                        .getPlatformBiome())
-                                  .getDelegate()
-                                  .getKey()
-                                  .orElseThrow())
-                .orElseThrow();
-                
-         */
         return biomeRegistry.entryOf(((ProtoPlatformBiome) pack
                 .getBiomeProvider()
                 .getBiome(biomeX << 2, biomeZ << 2, seed)
