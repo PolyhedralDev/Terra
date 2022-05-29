5c165cd40d13d6d726d2f06a2e6457694d2d9ec1
dfsek <dfsek@protonmail.com>
2022-05-29 04:51:25 -0700

    fix chunk generator wrapper codec

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/data/Codecs.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/data/Codecs.java
@@ -1,19 +1,16 @@
 package com.dfsek.terra.fabric.data;
 
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.Lifecycle;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.structure.StructureSet;
-import net.minecraft.util.dynamic.RegistryOps;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryCodecs;
-import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
-
 import com.dfsek.terra.api.config.ConfigPack;
 import com.dfsek.terra.api.registry.key.RegistryKey;
 import com.dfsek.terra.fabric.FabricEntryPoint;
 import com.dfsek.terra.fabric.generation.FabricChunkGeneratorWrapper;
 import com.dfsek.terra.fabric.generation.TerraBiomeSource;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.dynamic.RegistryOps;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.gen.chunk.ChunkGeneratorSettings;
 
 
 public final class Codecs {
@@ -52,10 +49,10 @@ public final class Codecs {
     
     public static final Codec<FabricChunkGeneratorWrapper> FABRIC_CHUNK_GENERATOR_WRAPPER = RecordCodecBuilder.create(
             instance -> instance.group(
-                    RegistryCodecs.dynamicRegistry(Registry.STRUCTURE_SET_KEY, Lifecycle.stable(), StructureSet.CODEC)
-                                  .fieldOf("structures")
-                                  .stable()
-                                  .forGetter(FabricChunkGeneratorWrapper::getNoiseRegistry),
+                    RegistryOps.createRegistryCodec(Registry.STRUCTURE_SET_KEY)
+                               .fieldOf("structure_registry")
+                               .stable()
+                               .forGetter(FabricChunkGeneratorWrapper::getNoiseRegistry),
                     TERRA_BIOME_SOURCE.fieldOf("biome_source")
                                       .stable()
                                       .forGetter(FabricChunkGeneratorWrapper::getBiomeSource),
