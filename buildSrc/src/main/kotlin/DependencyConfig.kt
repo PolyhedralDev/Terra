8977e82b736ab86aabd9184c0c9bb5c136e4f5e7
dfsek <dfsek@protonmail.com>
2022-05-29 15:05:29 -0700

    add Bukkit NMS dependency

--- buildSrc/src/main/kotlin/DependencyConfig.kt
+++ buildSrc/src/main/kotlin/DependencyConfig.kt
@@ -3,6 +3,7 @@ import org.gradle.kotlin.dsl.creating
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.getting
+import org.gradle.kotlin.dsl.maven
 import org.gradle.kotlin.dsl.repositories
 
 fun Project.configureDependencies() {
@@ -29,9 +30,10 @@ fun Project.configureDependencies() {
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://maven.fabricmc.net/") }
-        maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
-        maven { url = uri("https://repo.codemc.org/repository/maven-public") }
+        maven("https://maven.fabricmc.net/")
+        maven("https://repo.codemc.org/repository/maven-public")
+        maven("https://repo.codemc.io/repository/nms/")
+        maven("https://papermc.io/repo/repository/maven-public/")
     }
     
     dependencies {
