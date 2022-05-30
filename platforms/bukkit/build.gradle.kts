126ddaf9c783440953aa2dfefe02451fe3df0285
dfsek <dfsek@protonmail.com>
2022-05-29 20:42:11 -0700

    dont shade unneeded bukkit dependencies

--- platforms/bukkit/build.gradle.kts
+++ platforms/bukkit/build.gradle.kts
@@ -185,6 +185,13 @@ tasks.named<ShadowJar>("shadowJar") {
     relocate("io.papermc.lib", "com.dfsek.terra.lib.paperlib")
     relocate("com.google.common", "com.dfsek.terra.lib.google.common")
     relocate("org.apache.logging.slf4j", "com.dfsek.terra.lib.slf4j-over-log4j")
+    exclude("org/slf4j/**")
+    exclude("org/checkerframework/**")
+    exclude("org/jetbrains/annotations/**")
+    exclude("org/intellij/**")
+    exclude("com/google/errorprone/**")
+    exclude("com/google/j2objc/**")
+    exclude("javax/**")
 }
 
 
