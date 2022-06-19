33ca98ccaf52a611912fb5a8ee5afc60dd2397bc
dfsek <dfsek@protonmail.com>
2022-06-19 01:01:12 -0700

    dont shade caffeine into pipeline addon

--- common/addons/biome-provider-pipeline/build.gradle.kts
+++ common/addons/biome-provider-pipeline/build.gradle.kts
@@ -2,13 +2,11 @@ version = version("1.0.1")
 
 dependencies {
     compileOnlyApi(project(":common:addons:manifest-addon-loader"))
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.0")
     
     implementation("net.jafama", "jafama", Versions.Libraries.Internal.jafama)
     testImplementation("net.jafama", "jafama", Versions.Libraries.Internal.jafama)
 }
 
 tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
-    relocate("com.github.benmanes.caffeine", "com.dfsek.terra.addons.biome.pipeline.lib.caffeine")
     relocate("net.jafama", "com.dfsek.terra.addons.biome.pipeline.lib.jafama")
 }
