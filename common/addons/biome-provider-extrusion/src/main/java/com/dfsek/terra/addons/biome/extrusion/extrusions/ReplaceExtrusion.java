41fc28e1e94eafdd8b6360467557cc2fb5adf61c
dfsek <dfsek@protonmail.com>
2022-06-19 00:34:03 -0700

    use biome query API in ReplaceExtrusion

--- common/addons/biome-provider-extrusion/src/main/java/com/dfsek/terra/addons/biome/extrusion/extrusions/ReplaceExtrusion.java
+++ common/addons/biome-provider-extrusion/src/main/java/com/dfsek/terra/addons/biome/extrusion/extrusions/ReplaceExtrusion.java
@@ -2,6 +2,7 @@ package com.dfsek.terra.addons.biome.extrusion.extrusions;
 
 import com.dfsek.terra.addons.biome.extrusion.api.Extrusion;
 import com.dfsek.terra.addons.biome.extrusion.api.ReplaceableBiome;
+import com.dfsek.terra.addons.biome.query.api.BiomeQueries;
 import com.dfsek.terra.api.noise.NoiseSampler;
 import com.dfsek.terra.api.util.Range;
 import com.dfsek.terra.api.util.collection.ProbabilityCollection;
@@ -22,19 +23,21 @@ public class ReplaceExtrusion implements Extrusion {
     
     private final ProbabilityCollection<ReplaceableBiome> biomes;
     
-    private final String tag;
+    private final Predicate<Biome> hasTag;
     
     public ReplaceExtrusion(NoiseSampler sampler, Range range, ProbabilityCollection<ReplaceableBiome> biomes, String tag) {
         this.sampler = sampler;
         this.range = range;
         this.biomes = biomes;
-        this.tag = tag;
+        this.hasTag = BiomeQueries.has(tag);
     }
     
     @Override
     public Biome extrude(Biome original, int x, int y, int z, long seed) {
-        if(!original.getTags().contains(tag)) return original;
-        return range.ifInRange(y, () -> biomes.get(sampler, x, y, z, seed).get(original), original);
+        if(hasTag.test(original)) {
+            return range.ifInRange(y, () -> biomes.get(sampler, x, y, z, seed).get(original), original);
+        }
+        return original;
     }
     
     @Override
