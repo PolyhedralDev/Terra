08299602320517d371528541518e4f307dc14f0a
dfsek <dfsek@protonmail.com>
2022-06-19 23:08:52 -0700

    simplify Fabric buildscript

--- platforms/fabric/build.gradle.kts
+++ platforms/fabric/build.gradle.kts
@@ -1,10 +1,5 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import com.modrinth.minotaur.TaskModrinthUpload
-import net.fabricmc.loom.task.RemapJarTask
-
 plugins {
     id("fabric-loom") version Versions.Fabric.loom
-    id("com.modrinth.minotaur") version Versions.Fabric.minotaur
     id("io.github.juuxel.loom-quiltflower") version Versions.Fabric.loomQuiltflower
 }
 
@@ -38,44 +33,21 @@ loom {
 }
 
 
-
 addonDir(project.file("./run/config/Terra/addons"), tasks.named("runClient").get())
 addonDir(project.file("./run/config/Terra/addons"), tasks.named("runServer").get())
 
-tasks.withType<JavaCompile>().configureEach {
-    options.release.set(17)
-}
 
-tasks.getByName<ShadowJar>("shadowJar") {
-    exclude("org/slf4j/**")
-}
+tasks {
+    compileJava {
+        options.release.set(17)
+    }
 
-val remapped = tasks.register<RemapJarTask>("remapShadedJar") {
-    dependsOn("installAddons")
-    group = "fabric"
-    val shadowJar = tasks.getByName<ShadowJar>("shadowJar")
-    dependsOn(shadowJar)
-    inputFile.set(shadowJar.archiveFile)
-    archiveFileName.set(shadowJar.archiveFileName.get().replace(Regex("-shaded\\.jar$"), "-shaded-mapped.jar"))
-    addNestedDependencies.set(true)
-}
-
-tasks.named("assemble").configure {
-    dependsOn("remapShadedJar")
-}
-
-tasks.withType<Jar> {
-    finalizedBy(remapped)
-}
-
-tasks.register<TaskModrinthUpload>("publishModrinth") {
-    dependsOn("remapShadedJar")
-    group = "fabric"
-    token = System.getenv("MODRINTH_SECRET")
-    projectId = "FIlZB9L0"
-    versionNumber = "${project.version}-fabric"
-    uploadFile = remapped.get().archiveFile.get().asFile
-    releaseType = "beta"
-    addGameVersion(Versions.Fabric.minecraft)
-    addLoader("fabric")
+    shadowJar {
+        exclude("org/slf4j/**")
+    }
+    
+    remapJar {
+        inputFile.set(shadowJar.get().archiveFile)
+        archiveFileName.set("${rootProject.name.capitalize()}-${project.version}.jar")
+    }
 }
