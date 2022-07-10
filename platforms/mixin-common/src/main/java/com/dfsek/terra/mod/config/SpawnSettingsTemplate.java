9b41af8a57a14082072c7c4434798ae766b487c4
Zoë <duplexsys@protonmail.com>
2022-07-10 16:45:51 -0700

    UX tweaks to biome config

--- platforms/mixin-common/src/main/java/com/dfsek/terra/mod/config/SpawnSettingsTemplate.java
+++ platforms/mixin-common/src/main/java/com/dfsek/terra/mod/config/SpawnSettingsTemplate.java
@@ -3,7 +3,9 @@ package com.dfsek.terra.mod.config;
 import com.dfsek.tectonic.api.config.template.annotations.Default;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
 import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
+import net.minecraft.entity.SpawnGroup;
 import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.biome.SpawnSettings.SpawnEntry;
 
 import java.util.List;
 
@@ -25,7 +27,10 @@ public class SpawnSettingsTemplate implements ObjectTemplate<SpawnSettings> {
     public SpawnSettings get() {
         SpawnSettings.Builder builder = new SpawnSettings.Builder();
         for(SpawnTypeConfig spawn : spawns) {
-            builder.spawn(spawn.getGroup(), spawn.getEntry());
+            SpawnGroup group = spawn.getGroup();
+            for (SpawnEntry entry : spawn.getEntry()) {
+                builder.spawn(group, entry);
+            }
         }
         for(SpawnCostConfig cost : costs) {
             builder.spawnCost(cost.getType(), cost.getMass(), cost.getGravity());
