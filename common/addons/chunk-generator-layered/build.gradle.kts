c2902cc549d0b5e2b0c7dfb39318498123e00f04
Astrash <astrash@protonmail.com>
2022-07-30 12:01:10 +1000

    Add paralithic as layered gen dependency

--- common/addons/chunk-generator-layered/build.gradle.kts
+++ common/addons/chunk-generator-layered/build.gradle.kts
@@ -2,6 +2,7 @@ version = version("0.1.0")
 
 dependencies {
     compileOnlyApi(project(":common:addons:manifest-addon-loader"))
+    api("com.dfsek", "paralithic", Versions.Libraries.paralithic)
     
     implementation("net.jafama", "jafama", Versions.Libraries.Internal.jafama)
     testImplementation("net.jafama", "jafama", Versions.Libraries.Internal.jafama)
@@ -9,4 +10,5 @@ dependencies {
 
 tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar") {
     relocate("net.jafama", "com.dfsek.terra.addons.chunkgenerator.lib.jafama")
+    relocate("com.dfsek.paralithic", "com.dfsek.terra.addons.chunkgenerator.lib.paralithic")
 }
