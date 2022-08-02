586ab7d89c5e2ea483eef533ebc876981abb05f6
Zoë <duplexsys@protonmail.com>
2022-08-01 17:38:30 -0700

    better res fix (also I don't think it worked before)

--- common/addons/biome-provider-pipeline/src/main/java/com/dfsek/terra/addons/biome/pipeline/BiomePipelineProvider.java
+++ common/addons/biome-provider-pipeline/src/main/java/com/dfsek/terra/addons/biome/pipeline/BiomePipelineProvider.java
@@ -81,11 +81,13 @@ public class BiomePipelineProvider implements BiomeProvider {
     public Biome getBiome(int x, int z, long seed) {
         x += mutator.noise(seed + 1, x, z) * noiseAmp;
         z += mutator.noise(seed + 2, x, z) * noiseAmp;
-
-
+        
         x /= resolution;
         z /= resolution;
-
+        
+        x *= resolution;
+        z *= resolution;
+        
         int fdX = FastMath.floorDiv(x, pipeline.getSize());
         int fdZ = FastMath.floorDiv(z, pipeline.getSize());
         return holderCache.get(new SeededVector(fdX, fdZ, seed)).getBiome(x - fdX * pipeline.getSize(),
