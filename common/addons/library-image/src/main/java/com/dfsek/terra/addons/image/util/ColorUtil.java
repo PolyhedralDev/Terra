4e225a6592622e7f8b476abbd8dad667e9703350
Astrash <astrash@protonmail.com>
2022-11-24 13:55:36 +1100

    Add utility methods for zero-ing color channels

--- common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/util/ColorUtil.java
+++ common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/util/ColorUtil.java
@@ -33,11 +33,44 @@ public class ColorUtil {
         return (getRed(rgb) + getGreen(rgb) + getBlue(rgb)) / 3;
     }
     
+    public static int getChannel(int rgb, Channel channel) {
+        return channel.from(rgb);
+    }
+    
+    public static int zeroRed(int rgb) {
+        return rgb & ~0x00FF0000;
+    }
+    
+    public static int zeroGreen(int rgb) {
+        return rgb & ~0x0000FF00;
+    }
+    
+    public static int zeroBlue(int rgb) {
+        return rgb & ~0x000000FF;
+    }
+    
+    public static int zeroAlpha(int rgb) {
+        return rgb & ~0xFF000000;
+    }
+    
+    public static int zeroGrayscale(int rgb) {
+        return rgb & ~0x00FFFFFF;
+    }
+    
+    public static int zeroChannel(int rgb, Channel channel) {
+        return channel.zero(rgb);
+    }
+    
     public enum Channel {
         RED {
             @Override
             public int from(int rgb) {
                 return getRed(rgb);
+            }
+    
+            @Override
+            public int zero(int rgb) {
+                return zeroRed(rgb);
             }
         },
         GREEN {
@@ -45,11 +78,21 @@ public class ColorUtil {
             public int from(int rgb) {
                 return getGreen(rgb);
             }
+    
+            @Override
+            public int zero(int rgb) {
+                return zeroGreen(rgb);
+            }
         },
         BLUE {
             @Override
             public int from(int rgb) {
                 return getBlue(rgb);
+            }
+    
+            @Override
+            public int zero(int rgb) {
+                return zeroBlue(rgb);
             }
         },
         GRAYSCALE {
@@ -57,14 +100,26 @@ public class ColorUtil {
             public int from(int rgb) {
                 return getGrayscale(rgb);
             }
+    
+            @Override
+            public int zero(int rgb) {
+                return zeroGrayscale(rgb);
+            }
         },
         ALPHA {
             @Override
             public int from(int rgb) {
                 return getAlpha(rgb);
             }
+    
+            @Override
+            public int zero(int rgb) {
+                return zeroAlpha(rgb);
+            }
         };
         
         public abstract int from(int rgb);
+        
+        public abstract int zero(int rgb);
     }
 }
