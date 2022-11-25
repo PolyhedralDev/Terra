b3f072d6897c6508831f1735470b6f8a09eeb186
Astrash <astrash@protonmail.com>
2022-11-25 14:11:16 +1100

    Properly ignore alpha of lookup map

--- common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/converter/ExactColorConverter.java
+++ common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/converter/ExactColorConverter.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Set;
 
 import com.dfsek.terra.addons.image.util.ColorUtil;
+import com.dfsek.terra.addons.image.util.MapUtil;
 
 
 public class ExactColorConverter<T> implements ColorConverter<T> {
@@ -15,6 +16,9 @@ public class ExactColorConverter<T> implements ColorConverter<T> {
     private final boolean ignoreAlpha;
     
     public ExactColorConverter(Map<Integer, T> map, T fallback, boolean ignoreAlpha) {
+        if (ignoreAlpha) {
+            map = MapUtil.mapKeys(map, ColorUtil::zeroAlpha);
+        }
         this.map = map;
         this.fallback = fallback;
         this.ignoreAlpha = ignoreAlpha;
