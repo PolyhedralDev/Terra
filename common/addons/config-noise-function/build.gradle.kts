9514641e1e67ba6c4d58543c74f51f701add45e9
Astrash <astrash@protonmail.com>
2023-02-07 10:17:04 +1100

    Revert "Bump config-noise-function major version"
    This reverts commit 68875cc17bd4bbe78dcdc3af06afb5723182732f.

--- common/addons/config-noise-function/build.gradle.kts
+++ common/addons/config-noise-function/build.gradle.kts
@@ -1,6 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-version = version("2.0.0")
+version = version("1.0.0")
 
 dependencies {
     compileOnlyApi(project(":common:addons:manifest-addon-loader"))
