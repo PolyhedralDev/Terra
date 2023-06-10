183255140b746754aa617e4021cf9037ad99660d
dfsek <dfsek@dfsek.com>
2023-06-10 15:28:46 -0700

    remove unused RegistrarInstance class

--- platforms/mixin-common/src/main/java/com/dfsek/terra/mod/mixin_ifaces/RegistrarInstance.java
+++
@@ -1,8 +0,0 @@
-package com.dfsek.terra.mod.mixin_ifaces;
-
-import net.minecraft.world.gen.WorldPresets;
-
-
-public class RegistrarInstance {
-    public static WorldPresets.Registrar INSTANCE;
-}
