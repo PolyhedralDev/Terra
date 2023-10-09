b5e7c7c1127195a73e09494002e38ce9d14a2435
Astrash <astrash@protonmail.com>
2023-10-10 09:44:53 +1100

    Fix TOP locator

--- common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/locators/TopLocator.java
+++ common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/locators/TopLocator.java
@@ -22,7 +22,7 @@ public class TopLocator implements Locator {
     
     @Override
     public BinaryColumn getSuitableCoordinates(Column<?> column) {
-        for(int y : search) {
+        for(int y = search.getMax(); y >= search.getMin(); y--) {
             if(column.getBlock(y).isAir() && !column.getBlock(y - 1).isAir()) {
                 return new BinaryColumn(y, y + 1, yi -> true);
             }
