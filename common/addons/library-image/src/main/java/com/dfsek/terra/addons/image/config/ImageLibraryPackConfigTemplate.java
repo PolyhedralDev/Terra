e30bcbf1ba63f8291a2d441604f06cfc902e8e8c
Astrash <astrash@protonmail.com>
2023-10-24 13:26:35 +1100

    Improve image caching options

---
+++ common/addons/library-image/src/main/java/com/dfsek/terra/addons/image/config/ImageLibraryPackConfigTemplate.java
@@ -0,0 +1,41 @@
+package com.dfsek.terra.addons.image.config;
+
+import com.dfsek.tectonic.api.config.template.ConfigTemplate;
+
+import com.dfsek.tectonic.api.config.template.annotations.Default;
+import com.dfsek.tectonic.api.config.template.annotations.Description;
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+
+import com.dfsek.terra.api.properties.Properties;
+
+public class ImageLibraryPackConfigTemplate implements ConfigTemplate, Properties {
+    // TODO - These would be better as plugin wide config parameters in config.yml
+    
+    @Value("images.cache.load-on-use")
+    @Description("If set to true, images will load into memory upon use rather than on pack load.")
+    @Default
+    private boolean loadOnUse = false;
+    
+    @Value("images.cache.unload-on-timeout")
+    @Description("If set to true, images will be removed from memory if not used after a timeout, otherwise images will stay loaded in memory. " +
+                 "Trades decreased memory consumption when not performing image reads for extra processing time required to perform cache lookups.")
+    @Default
+    private boolean unloadOnTimeout = false;
+    
+    @Value("images.cache.timeout")
+    @Description("How many seconds to keep images loaded in the image cache for if images.cache.unload-on-timeout is enabled.")
+    @Default
+    private int cacheTimeout = 300;
+    
+    public boolean loadOnUse() {
+        return loadOnUse;
+    }
+    
+    public boolean unloadOnTimeout() {
+        return unloadOnTimeout;
+    }
+    
+    public int getCacheTimeout() {
+        return cacheTimeout;
+    }
+}
