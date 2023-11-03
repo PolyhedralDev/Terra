c98d39c5d7ce4dc7aa9e496fa3e50d8120439e57
Zoë Gidiere <duplexsys@protonmail.com>
2023-11-02 18:42:51 -0600

    Update licence year

--- common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/config/AndLocatorTemplate.java
+++ common/addons/config-locators/src/main/java/com/dfsek/terra/addons/feature/locator/config/AndLocatorTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Polyhedral Development
+ * Copyright (c) 2020-2023 Polyhedral Development
  *
  * The Terra Core Addons are licensed under the terms of the MIT License. For more details,
  * reference the LICENSE file in this module's root directory.
--- common/api/src/main/java/com/dfsek/terra/api/block/BlockType.java
+++ common/api/src/main/java/com/dfsek/terra/api/block/BlockType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Polyhedral Development
+ * Copyright (c) 2020-2023 Polyhedral Development
  *
  * The Terra API is licensed under the terms of the MIT License. For more details,
  * reference the LICENSE file in the common/api directory.
