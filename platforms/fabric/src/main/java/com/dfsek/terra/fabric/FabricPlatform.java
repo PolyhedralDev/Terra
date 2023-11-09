a117db40be9f8d495435fe4c9f47cbc1c6868b63
Zoë Gidiere <duplexsys@protonmail.com>
2023-11-09 12:47:48 -0700

    change formatting

--- platforms/fabric/src/main/java/com/dfsek/terra/fabric/FabricPlatform.java
+++ platforms/fabric/src/main/java/com/dfsek/terra/fabric/FabricPlatform.java
@@ -33,8 +33,8 @@ public class FabricPlatform extends LifecyclePlatform {
     @Override
     protected Collection<BaseAddon> getPlatformMods() {
         return FabricLoader.getInstance().getAllMods().stream().flatMap(
-                mod -> parseModData(mod.getMetadata().getId(), mod.getMetadata().getVersion().getFriendlyString(), "fabric")).collect(
-                Collectors.toList());
+                mod -> parseModData(mod.getMetadata().getId(), mod.getMetadata().getVersion().getFriendlyString(), "fabric")
+                                                                       ).collect(Collectors.toList());
     }
     
     @Override
