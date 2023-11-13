fd0c4df04ca7a69561bc09a6eb565333708bb941
Zoë Gidiere <duplexsys@protonmail.com>
2023-11-12 21:18:27 -0700

    Revert "add stub for not physics"
    This reverts commit 839bc23a5a2ee5919b7c99620b1d14bdd61923c1.

--- common/addons/structure-terrascript-loader/src/main/java/com/dfsek/terra/addons/terrascript/script/functions/BlockFunction.java
+++ common/addons/structure-terrascript-loader/src/main/java/com/dfsek/terra/addons/terrascript/script/functions/BlockFunction.java
@@ -39,18 +39,6 @@ public class BlockFunction implements Function<Void> {
     private final Position position;
 
     public BlockFunction(Returnable<Number> x, Returnable<Number> y, Returnable<Number> z, Returnable<String> blockData,
-                         Returnable<Boolean> overwrite, Platform platform, Position position) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.blockData = blockData;
-        this.overwrite = overwrite;
-        this.platform = platform;
-        this.position = position;
-        this.physics = new BooleanConstant(false, position);
-    }
-
-    public BlockFunction(Returnable<Number> x, Returnable<Number> y, Returnable<Number> z, Returnable<String> blockData,
                          Returnable<Boolean> overwrite, Returnable<Boolean> physics, Platform platform, Position position) {
         this.x = x;
         this.y = y;
