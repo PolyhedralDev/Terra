0efb0916e6d060851988d4fea2acdd1a7e8ce027
Zoë Gidiere <duplexsys@protonmail.com>
2023-11-17 16:16:18 -0700

    Initial Bukkit Biome Config

--- platforms/bukkit/nms/v1_20_R2/src/main/java/com/dfsek/terra/bukkit/nms/v1_20_R2/config/SpawnSettingsTemplate.java
+++ platforms/bukkit/nms/v1_20_R2/src/main/java/com/dfsek/terra/bukkit/nms/v1_20_R2/config/SpawnSettingsTemplate.java
@@ -1,59 +1,57 @@
-package com.dfsek.terra.mod.config;
+package com.dfsek.terra.bukkit.nms.v1_20_R2.config;
 
 import com.dfsek.tectonic.api.config.template.annotations.Default;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
 import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
-import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.SpawnSettings;
-import net.minecraft.world.biome.SpawnSettings.SpawnEntry;
+import java.util.List;
+import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
 
+public class SpawnSettingsTemplate implements ObjectTemplate<MobSpawnSettings> {
 
-public class SpawnSettingsTemplate implements ObjectTemplate<SpawnSettings> {
-    
     private static final Logger logger = LoggerFactory.getLogger(SpawnTypeConfig.class);
     private static boolean used = false;
 
     @Value("spawns")
     @Default
     private List<SpawnTypeConfig> spawns = null;
-
+    
     @Value("costs")
     @Default
     private List<SpawnCostConfig> costs = null;
-
+    
     @Value("probability")
     @Default
     private Float probability = null;
-
+    
     @Override
-    public SpawnSettings get() {
-        SpawnSettings.Builder builder = new SpawnSettings.Builder();
+    public MobSpawnSettings get() {
+        MobSpawnSettings.Builder builder = new MobSpawnSettings.Builder();
         for(SpawnTypeConfig spawn : spawns) {
-            SpawnGroup group = spawn.getGroup();
+            MobCategory group = spawn.getGroup();
             if (spawn.getEntries() != null) {
-                for (SpawnEntry entry : spawn.getEntries()) {
-                    builder.spawn(group, entry);
+                for(SpawnerData entry : spawn.getEntries()) {
+                    builder.addSpawn(group, entry);
                 }
             } else if (spawn.getEntry() != null) {
                 if(!used) {
                     logger.warn("The entry sub-field of spawns is deprecated. " +
-                                "It is recommended to use the entries sub-field instead ");
+                                "It is recommended to use the entries sub-field instead.");
                     used = true;
                 }
-                builder.spawn(group, spawn.getEntry());
             }
         }
         for(SpawnCostConfig cost : costs) {
-            builder.spawnCost(cost.getType(), cost.getMass(), cost.getGravity());
+            builder.addMobCharge(cost.getType(), cost.getMass(), cost.getGravity());
         }
         if(probability != null) {
-            builder.creatureSpawnProbability(probability);
+            builder.creatureGenerationProbability(probability);
         }
-
+        
         return builder.build();
     }
 }
