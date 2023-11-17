81636f9d98ffbbcfa6e2d053b0dc1b53175ecedf
Zoë Gidiere <duplexsys@protonmail.com>
2023-11-17 16:16:18 -0700

    Initial Bukkit Biome Config

---
+++ platforms/bukkit/nms/v1_20_R2/src/main/java/com/dfsek/terra/bukkit/nms/v1_20_R2/config/EntityTypeTemplate.java
@@ -0,0 +1,21 @@
+package com.dfsek.terra.bukkit.nms.v1_20_R2.config;
+
+import com.dfsek.tectonic.api.config.template.annotations.Default;
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+
+
+public class EntityTypeTemplate implements ObjectTemplate<EntityType<?>> {
+    @Value("id")
+    @Default
+    private ResourceLocation id = null;
+    
+    @Override
+    public EntityType<?> get() {
+        return BuiltInRegistries.ENTITY_TYPE.get(id);
+    }
+}
---
+++ platforms/bukkit/nms/v1_20_R2/src/main/java/com/dfsek/terra/bukkit/nms/v1_20_R2/config/SpawnEntryTemplate.java
@@ -0,0 +1,31 @@
+package com.dfsek.terra.bukkit.nms.v1_20_R2.config;
+
+import com.dfsek.tectonic.api.config.template.annotations.Default;
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.level.biome.MobSpawnSettings.SpawnerData;
+
+
+public class SpawnEntryTemplate implements ObjectTemplate<SpawnerData> {
+    @Value("type")
+    @Default
+    private EntityType<?> type = null;
+    
+    @Value("weight")
+    @Default
+    private Integer weight = null;
+    
+    @Value("min-group-size")
+    @Default
+    private Integer minGroupSize = null;
+    
+    @Value("max-group-size")
+    @Default
+    private Integer maxGroupSize = null;
+    
+    @Override
+    public SpawnerData get() {
+        return new SpawnerData(type, weight, minGroupSize, maxGroupSize);
+    }
+}
