3aef977384d6d6f2d03b3ac2d33876634fe7f9a9
Astrashh <astrash@protonmail.com>
2023-11-25 12:02:45 +1100

    Patch version 6.4.1 (#435)
    * Bump version to 6.4.1
    
    * fix fabric dev env
    
    * Invert exposed ore logic (#433)
    
    * Invert exposed ore logic
    
    * Bump ore addon version
    
    * Use logger in Gradle over println (#434)
    
    * Log info instead of println in gradle scripts
    
    * Missed buildSrc printlns
    
    ---------
    
    Co-authored-by: Zoë <duplexsys@protonmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 preRelease(true)
 
-versionProjects(":common:api", version("6.4.0"))
-versionProjects(":common:implementation", version("6.4.0"))
-versionProjects(":platforms", version("6.4.0"))
+versionProjects(":common:api", version("6.4.1"))
+versionProjects(":common:implementation", version("6.4.1"))
+versionProjects(":platforms", version("6.4.1"))
 
 
 allprojects {
