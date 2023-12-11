597925480847bc3e4df5e4292d5bc7d71af50b09
Zoë Gidiere <duplexsys@protonmail.com>
2023-12-11 14:54:27 -0700

    wip

---
+++ common/implementation/base/src/main/java/com/dfsek/terra/config/pack/MetaPackTemplate.java
@@ -0,0 +1,42 @@
+package com.dfsek.terra.config.pack;
+
+import ca.solostudios.strata.version.Version;
+import com.dfsek.tectonic.api.config.template.ConfigTemplate;
+import com.dfsek.tectonic.api.config.template.annotations.Default;
+import com.dfsek.tectonic.api.config.template.annotations.Value;
+
+import com.dfsek.terra.api.config.ConfigPack;
+
+import java.util.Map;
+
+
+public class MetaPackTemplate implements ConfigTemplate {
+    @Value("id")
+    private String id;
+
+    @Value("author")
+    @Default
+    private String author = "Anon Y. Mous";
+
+    @Value("version")
+    private Version version;
+
+    @Value("packs")
+    private Map<String, String> packs;
+
+    public String getID() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public Version getVersion() {
+        return version;
+    }
+
+    public Map<String, String> getPacks() {
+        return packs;
+    }
+}
