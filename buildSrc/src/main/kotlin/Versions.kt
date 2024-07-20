44d23573b3c2c4f19f8b56492ac9c8218a83cfac
Oak <cooldaveb@live.co.uk>
2024-07-20 20:17:25 +0100

    Updated dependencies to Paper 1.21 snapshot

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -53,7 +53,7 @@ object Versions {
     
     object Bukkit {
         const val minecraft = "1.21"
-        const val paperBuild = "$minecraft-R0.1-20240615.005026-1"
+        const val paperBuild = "$minecraft-R0.1-SNAPSHOT"
         const val paper = paperBuild
         const val paperLib = "1.0.8"
         const val reflectionRemapper = "0.1.1"
