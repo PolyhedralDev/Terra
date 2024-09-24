f605ee1937f936617358732815776ad6561ec123
Zoe Gidiere <duplexsys@protonmail.com>
2024-09-23 22:35:01 -0600

    Remove Final from psuedoerosion template

--- common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/config/templates/noise/PseudoErosionTemplate.java
+++ common/addons/config-noise-function/src/main/java/com/dfsek/terra/addons/noise/config/templates/noise/PseudoErosionTemplate.java
@@ -15,55 +15,55 @@ public class PseudoErosionTemplate extends NoiseTemplate<PseudoErosionSampler> {
 
     @Value("octaves")
     @Default
-    private final int octaves = 4;
+    private int octaves = 4;
 
     @Value("lacunarity")
     @Default
-    private final double lacunarity = 2.0;
+    private double lacunarity = 2.0;
 
     @Value("gain")
     @Default
-    private final double gain = 0.5;
+    private double gain = 0.5;
 
     @Value("slope-strength")
     @Default
-    private final double slopeStrength = 1.0;
+    private double slopeStrength = 1.0;
 
 
     @Value("branch-strength")
     @Default
-    private final double branchStrength = 1.0;
+    private double branchStrength = 1.0;
 
     @Value("strength")
     @Default
-    private final double strength = 0.04;
+    private double strength = 0.04;
 
     @Value("erosion-frequency")
     @Default
-    private final double erosionFrequency = 0.02;
+    private double erosionFrequency = 0.02;
 
     @Value("sampler")
     private DerivativeNoiseSampler heightSampler;
 
     @Value("slope-mask.enable")
     @Default
-    private final boolean slopeMask = true;
+    private boolean slopeMask = true;
 
     @Value("slope-mask.none")
     @Default
-    private final double slopeMaskNone = -0.5;
+    private double slopeMaskNone = -0.5;
 
     @Value("slope-mask.full")
     @Default
-    private final double slopeMaskFull = 1;
+    private double slopeMaskFull = 1;
 
     @Value("jitter")
     @Default
-    private final double jitterModifier = 1;
+    private double jitterModifier = 1;
 
     @Value("average-impulses")
     @Default
-    private final boolean averageErosionImpulses = true;
+    private boolean averageErosionImpulses = true;
 
     @Override
     public PseudoErosionSampler get() {
