56983a54f0a3c494e7b21409864824df69c73da3
daoge_cmd <3523206925@qq.com>
2024-10-14 20:36:40 +0800

    build: move repo declaration to DependencyConfig.kt

--- platforms/allay/build.gradle.kts
+++ platforms/allay/build.gradle.kts
@@ -1,10 +1,3 @@
-repositories {
-    maven("https://repo.opencollab.dev/maven-releases/")
-    maven("https://repo.opencollab.dev/maven-snapshots/")
-    maven("https://storehouse.okaeri.eu/repository/maven-public/")
-    maven("https://jitpack.io/")
-}
-
 dependencies {
     shadedApi(project(":common:implementation:base"))
     implementation("com.google.code.gson", "gson", "2.11.0")
