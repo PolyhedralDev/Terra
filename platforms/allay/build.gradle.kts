6261f0849c5983ac334b83ebc7b0b4b3f9a965a5
daoge_cmd <3523206925@qq.com>
2024-10-14 11:23:27 +0800

    feat: remove java version declaration in allay platform as all platforms are in java 21 now

--- platforms/allay/build.gradle.kts
+++ platforms/allay/build.gradle.kts
@@ -10,13 +10,7 @@ dependencies {
     implementation("com.google.code.gson", "gson", "2.11.0")
 
     compileOnly("org.projectlombok:lombok:1.18.32")
-    compileOnly(group = "org.allaymc.allay", name = "api", version = "master-SNAPSHOT")
+    compileOnly("org.allaymc.allay:api:master-SNAPSHOT")
 
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-}
-
-tasks {
-    compileJava {
-        options.release.set(21)
-    }
 }
