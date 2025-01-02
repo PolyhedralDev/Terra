32d4519a8fcd0a3f8c8f1805e1dadc241ad3a19b
Christian Bergschneider <christian.bergschneider@gmx.de>
2025-01-02 19:32:14 +0100

    feat: implement platform enchantments

--- platforms/minestom/src/main/java/com/dfsek/terra/minestom/item/MinestomItemHandle.java
+++ platforms/minestom/src/main/java/com/dfsek/terra/minestom/item/MinestomItemHandle.java
@@ -4,22 +4,30 @@ import com.dfsek.terra.api.handle.ItemHandle;
 import com.dfsek.terra.api.inventory.Item;
 import com.dfsek.terra.api.inventory.item.Enchantment;
 
+import net.minestom.server.MinecraftServer;
+
 import java.util.Set;
+import java.util.stream.Collectors;
 
 
 public class MinestomItemHandle implements ItemHandle {
     @Override
     public Item createItem(String data) {
-        return null;
+        return new MinestomMaterial(data);
     }
 
     @Override
     public Enchantment getEnchantment(String id) {
-        return null;
+        return new MinestomEnchantment(id);
     }
 
     @Override
     public Set<Enchantment> getEnchantments() {
-        return Set.of();
+        return MinecraftServer
+            .getEnchantmentRegistry()
+            .values()
+            .stream()
+            .map(MinestomEnchantment::new)
+            .collect(Collectors.toSet());
     }
 }
