f278ae7ab5da978d33d9e3dcaf6ac72fdf34f5ce
Zoë Gidiere <duplexsys@protonmail.com>
2025-02-26 22:34:41 -0700

    update licence

--- common/addons/biome-provider-pipeline-v2/src/main/java/com/dfsek/terra/addons/biome/pipeline/v2/config/source/SamplerSourceTemplate.java
+++ common/addons/biome-provider-pipeline-v2/src/main/java/com/dfsek/terra/addons/biome/pipeline/v2/config/source/SamplerSourceTemplate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024 Polyhedral Development
+ * Copyright (c) 2020-2025 Polyhedral Development
  *
  * The Terra Core Addons are licensed under the terms of the MIT License. For more details,
  * reference the LICENSE file in this module's root directory.
