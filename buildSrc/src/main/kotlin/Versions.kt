8bad33ef229964b89a153afd0a40dc631158c8a5
Zoe Gidiere <duplexsys@protonmail.com>
2025-02-27 19:33:51 -0700

    fabric 1.21.4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -28,7 +28,7 @@ object Versions {
     }
     
     object Fabric {
-        const val fabricAPI = "0.106.1+${Mod.minecraft}"
+        const val fabricAPI = "0.118.0+${Mod.minecraft}"
         const val cloud = "2.0.0-beta.9"
     }
 //
@@ -40,9 +40,9 @@ object Versions {
     object Mod {
         const val mixin = "0.15.3+mixin.0.8.7"
         
-        const val minecraft = "1.21.3"
-        const val yarn = "$minecraft+build.2"
-        const val fabricLoader = "0.16.7"
+        const val minecraft = "1.21.4"
+        const val yarn = "$minecraft+build.8"
+        const val fabricLoader = "0.16.10"
         
         const val architecuryLoom = "1.7.413"
         const val architecturyPlugin = "3.4.159"
