b4a9f4d3c03160437ab53d985eb372588f9af1c0
solonovamax <solonovamax@12oclockpoint.com>
2025-03-08 15:24:17 -0500

    Download allay mappings from github instead of using git submodules
    Signed-off-by: solonovamax <solonovamax@12oclockpoint.com>

--- platforms/allay/build.gradle.kts
+++ platforms/allay/build.gradle.kts
@@ -1,26 +1,37 @@
+repositories {
+    ivy {
+        url = uri("https://raw.githubusercontent.com/")
+        patternLayout {
+            artifact("[organisation]/[revision]/[artifact].([ext])")
+            setM2compatible(true)
+        }
+        metadataSources {
+            artifact()
+        }
+    }
+}
+
+val geyserMappings: Configuration by configurations.register("geyserMappings") {
+    isCanBeConsumed = false
+}
+
 dependencies {
     shadedApi(project(":common:implementation:base"))
 
     implementation("com.google.code.gson", "gson", Versions.Allay.gson)
 
     compileOnly("org.allaymc.allay", "api", Versions.Allay.api)
-}
 
-tasks.register<Copy>("copyMappings") {
-    from("src/main/resources/mappings") {
-        include("biomes.json", "items.json")
-    }
-    from("src/main/resources/mappings-generator") {
-        include("generator_blocks.json")
-        rename("generator_blocks.json", "blocks.json")
-    }
-    into("src/main/resources/mapping")
+    geyserMappings("GeyserMC.mappings:items:3626653@json")
+    geyserMappings("GeyserMC.mappings:biomes:3626653@json")
+    geyserMappings("GeyserMC.mappings-generator:generator_blocks:366618e@json")
 }
 
 tasks.processResources {
-    dependsOn("copyMappings")
+    from(geyserMappings) {
+        into("mapping")
 
-    exclude("mapping/.keep")
-    exclude("mappings/**")
-    exclude("mappings-generator/**")
+        // rather jank, but whatever
+        rename("(?:generator_)?([^-]+)-(.*)\\.json", "$1.json")
+    }
 }
