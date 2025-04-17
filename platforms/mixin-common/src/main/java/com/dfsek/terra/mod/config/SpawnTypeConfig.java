016961c19c41c2501737599175937ae40699beb1
OakLoaf <oak@beaconstudios.org>
2025-04-17 17:41:45 +0100

    Initial attempt at updating mixin-commons

--- platforms/mixin-common/src/main/java/com/dfsek/terra/mod/config/SpawnTypeConfig.java
+++ platforms/mixin-common/src/main/java/com/dfsek/terra/mod/config/SpawnTypeConfig.java
@@ -4,7 +4,6 @@ import com.dfsek.tectonic.api.config.template.annotations.Default;
 import com.dfsek.tectonic.api.config.template.annotations.Value;
 import com.dfsek.tectonic.api.config.template.object.ObjectTemplate;
 import net.minecraft.entity.SpawnGroup;
-import net.minecraft.world.biome.SpawnSettings.SpawnEntry;
 
 import java.util.List;
 
@@ -16,13 +15,13 @@ public class SpawnTypeConfig implements ObjectTemplate<SpawnTypeConfig> {
 
     @Value("entries")
     @Default
-    private List<SpawnEntry> entry = null;
+    private List<SpawnEntryConfig> entry = null;
 
     public SpawnGroup getGroup() {
         return group;
     }
 
-    public List<SpawnEntry> getEntry() {
+    public List<SpawnEntryConfig> getEntry() {
         return entry;
     }
 
