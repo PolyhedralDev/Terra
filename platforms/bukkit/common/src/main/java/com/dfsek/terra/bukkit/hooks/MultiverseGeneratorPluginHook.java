85b6dcc89186d4311b5187d129dd963421ef2407
OakLoaf <oak@beaconstudios.org>
2025-06-19 14:24:04 +0100

    Moved all code that requires Multiverse imports into MultiverseGeneratorPluginHook

--- platforms/bukkit/common/src/main/java/com/dfsek/terra/bukkit/hooks/MultiverseGeneratorPluginHook.java
+++ platforms/bukkit/common/src/main/java/com/dfsek/terra/bukkit/hooks/MultiverseGeneratorPluginHook.java
@@ -3,6 +3,7 @@ package com.dfsek.terra.bukkit.hooks;
 import com.dfsek.terra.api.Platform;
 import com.dfsek.terra.api.registry.key.Keyed;
 
+import org.mvplugins.multiverse.core.MultiverseCoreApi;
 import org.mvplugins.multiverse.core.world.generators.GeneratorPlugin;
 import org.mvplugins.multiverse.external.jetbrains.annotations.NotNull;
 import org.mvplugins.multiverse.external.jetbrains.annotations.Nullable;
@@ -43,5 +44,10 @@ public final class MultiverseGeneratorPluginHook implements GeneratorPlugin {
     @Override
     public @NotNull String getPluginName() {
         return "Terra";
+    }
+
+    public static void register(Platform platform) {
+        MultiverseCoreApi.get().getGeneratorProvider()
+            .registerGeneratorPlugin(new MultiverseGeneratorPluginHook(platform));
     }
 }
