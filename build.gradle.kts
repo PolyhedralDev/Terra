8cfa2e1467ab81a1c7437c5a377634be8ff7e4bc
Zoë Gidiere <duplexsys@protonmail.com>
2025-06-19 12:23:59 -0600

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 preRelease(true)
 
-versionProjects(":common:api", version("6.6.4"))
-versionProjects(":common:implementation", version("6.6.4"))
-versionProjects(":platforms", version("6.6.4"))
+versionProjects(":common:api", version("6.6.5"))
+versionProjects(":common:implementation", version("6.6.5"))
+versionProjects(":platforms", version("6.6.5"))
 
 
 allprojects {
