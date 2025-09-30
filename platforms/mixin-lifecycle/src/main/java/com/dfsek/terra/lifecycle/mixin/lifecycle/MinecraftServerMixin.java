93157f3f0e4b763c5a7ee376616b8e86737c8a2b
OakLoaf <oak@beaconstudios.org>
2025-09-30 18:51:25 +0100

    Fixed compilation errors

--- platforms/mixin-lifecycle/src/main/java/com/dfsek/terra/lifecycle/mixin/lifecycle/MinecraftServerMixin.java
+++ platforms/mixin-lifecycle/src/main/java/com/dfsek/terra/lifecycle/mixin/lifecycle/MinecraftServerMixin.java
@@ -1,14 +1,11 @@
 package com.dfsek.terra.lifecycle.mixin.lifecycle;
 
-import com.dfsek.terra.mod.CommonPlatform;
-import com.dfsek.terra.mod.ModPlatform;
-
 import com.mojang.datafixers.DataFixer;
 import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.SaveLoader;
-import net.minecraft.server.WorldGenerationProgressListenerFactory;
 import net.minecraft.util.ApiServices;
+import net.minecraft.world.chunk.ChunkLoadProgress;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -25,11 +22,11 @@ public class MinecraftServerMixin {
     @Inject(method = "<init>(Ljava/lang/Thread;Lnet/minecraft/world/level/storage/LevelStorage$Session;" +
                      "Lnet/minecraft/resource/ResourcePackManager;Lnet/minecraft/server/SaveLoader;Ljava/net/Proxy;" +
                      "Lcom/mojang/datafixers/DataFixer;Lnet/minecraft/util/ApiServices;" +
-                     "Lnet/minecraft/server/WorldGenerationProgressListenerFactory;)V",
+                     "Lnet/minecraft/world/chunk/ChunkLoadProgress;)V",
             at = @At("RETURN"))
     private void injectConstructor(Thread serverThread, LevelStorage.Session session, ResourcePackManager dataPackManager,
                                    SaveLoader saveLoader, Proxy proxy, DataFixer dataFixer, ApiServices apiServices,
-                                   WorldGenerationProgressListenerFactory worldGenerationProgressListenerFactory, CallbackInfo ci) {
+                                   ChunkLoadProgress chunkLoadProgress, CallbackInfo ci) {
         LifecyclePlatform.setServer((MinecraftServer) (Object) this);
     }
 }
