2b5d613aef332cb6ca800a22218c035a49e3cdc4
Zoë Gidiere <duplexsys@protonmail.com>
2025-10-03 00:54:53 -0600

    fix up

--- platforms/bukkit/nms/v1_21_9/src/main/java/com/dfsek/terra/bukkit/nms/v1_21_9/AwfulBukkitHacks.java
+++ platforms/bukkit/nms/v1_21_9/src/main/java/com/dfsek/terra/bukkit/nms/v1_21_9/AwfulBukkitHacks.java
@@ -29,7 +29,6 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import com.dfsek.terra.bukkit.nms.v1_21_7.config.VanillaBiomeProperties;
 import com.dfsek.terra.bukkit.world.BukkitBiomeInfo;
 import com.dfsek.terra.bukkit.world.BukkitPlatformBiome;
 import com.dfsek.terra.registry.master.ConfigRegistry;
