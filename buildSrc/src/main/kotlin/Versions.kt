b28b2bc754b7d1900d1e30ed2502b7eedc3d6eb8
Zoë Gidiere <duplexsys@protonmail.com>
2025-10-05 16:44:49 -0600

    Update seismic

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
         const val tectonic = "4.2.1"
         const val paralithic = "2.0.1"
         const val strata = "1.3.2"
-        const val seismic = "2.0.4"
+        const val seismic = "2.1.1"
         
         const val cloud = "2.0.0"
         
